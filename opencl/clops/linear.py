4c40bb785a202f9daf743637497135d25337d2ed
Li, Tingqian <tingqian.li@intel.com>
2024-10-24 17:50:37 +0800

    unroll opt for linear

--- opencl/clops/linear.py
+++ opencl/clops/linear.py
@@ -10,8 +10,9 @@ __kernel void Linear_f16(__global half * A, __global half * B, __global half * C
         float sum = 0;
         __global half * pA = A + m*K;
         __global half * pB = B + n*K;
-        for(int k = 0; k < K; k++) {
-            sum += pA[k] * pB[k];
+        for(int k = 0; k < K; k += 8) {
+            for(int unroll = 0; unroll < 8; unroll++)
+                sum += pA[k+unroll] * pB[k+unroll];
         }
         C[m*N + n] = sum;
     }
@@ -45,10 +46,10 @@ __kernel void Linear_quant_I4(__global half * src, __global uchar * dst, __globa
 
     half rs = 1.0f/s;
     for(int i = 0; i < group_size; i+=2, dst++) {
-        //uchar v0 = clamp((uchar)round(src[i] * rs - zp), (uchar)0, (uchar)15);
-        //uchar v1 = clamp((uchar)round(src[i+1] * rs - zp), (uchar)0, (uchar)15);
-        uchar v0 = round(src[i] * rs - zp);
-        uchar v1 = round(src[i + 1] * rs - zp);
+        uchar v0 = clamp((uchar)round(src[i] * rs - zp), (uchar)0, (uchar)15);
+        uchar v1 = clamp((uchar)round(src[i+1] * rs - zp), (uchar)0, (uchar)15);
+        //uchar v0 = round(src[i] * rs - zp);
+        //uchar v1 = round(src[i + 1] * rs - zp);
         *dst = (v1 << 4) | v0;
     }
 }
@@ -83,17 +84,17 @@ from . import cl
 import numpy as np
 from .utils import *
 
-cl_kernels = cl.kernels(cl_kernel_sources, "-D FMACNT=4 -D UNROLL=4")
+cl_kernels = cl.kernels(cl_kernel_sources, "-D FMACNT=4 -D UNROLL=4", "./dump")
 
 class Linear:
     # weight: [N, K]
     def __init__(self, weight, bias):
         assert(bias is None)
+        self.N, self.K = weight.shape
+
+        # self.weight = to_cl(weight.half().transpose(1, 0).contiguous()) # internally transpose to [K, N]
         self.weight = to_cl(weight.half())
         self.bias = to_cl(bias)
-
-        self.N = self.weight.shape[0]
-        self.K = self.weight.shape[1]
 
     def __call__(self, input):
         # shape inference
@@ -106,7 +107,7 @@ class Linear:
         
         #print(M, self.K, self.N,  input.shape, self.weight.shape, output.shape)
 
-        cl_kernels.enqueue("Linear_f16", [self.N, M], [32, 32], input, self.weight, output, M, self.K, self.N)
+        cl_kernels.enqueue("Linear_f16", [self.N, M], [128, 1], input, self.weight, output, M, self.K, self.N)
         
         return output
 
@@ -127,11 +128,15 @@ class Linear_woq_I4:
         self.scales = cl.tensor([N, K//group_size], np.dtype(np.float16)) # scales
         self.zps = cl.tensor([N, K//group_size], np.dtype(np.float16))    # zero-points
         cl_kernels.enqueue("Linear_quant_I4", [K//group_size, N], [1, 1], weight_half, self.weight_i4, self.scales, self.zps, N, K, group_size)
+        # why we need this finish is unknown, but it's required to avoid bugs
+        cl.finish()
 
         self.bias = to_cl(bias)
         self.group_size = group_size
         self.N = N
         self.K = K
+
+        assert((self.N % 128) == 0)
 
 
     def __call__(self, input):
@@ -143,6 +148,5 @@ class Linear_woq_I4:
 
         M = input.numel // self.K
         #print(M, self.K, self.N,  input.shape, self.weight.shape, output.shape)
-        cl_kernels.enqueue("Linear_woq_I4", [self.N, M], [1, 1], input, output, self.weight_i4, self.scales, self.zps, M, self.N, self.K, self.group_size)
-
+        cl_kernels.enqueue("Linear_woq_I4", [self.N, M], [128, 1], input, output, self.weight_i4, self.scales, self.zps, M, self.N, self.K, self.group_size)
         return output
