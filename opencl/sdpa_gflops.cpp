b7ea3318b4af0e27cc1b1463786946a89be6f228
ceciliapeng2011 <cecilia.peng@intel.com>
2024-10-25 13:40:41 +0800

    update sdpa

--- opencl/sdpa_gflops.cpp
+++ opencl/sdpa_gflops.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <fstream>
 #include <streambuf>
+#include <cmath>
 
 #define CL_HPP_ENABLE_EXCEPTIONS
 
@@ -33,45 +34,51 @@ int main(void) {
     const std::string kernel_name("sdpa_opt_multi_tokens_6761455398808095608_0_0__sa");
 
     // execution
+    #define SUBGROUP_SIZE 16
+    #define S 128    // head_size
+    #define SG_SCALE_FACTOR 2
+    #define SOURCE_SEQ_LEN_BLOCK_SIZE (S*SG_SCALE_FACTOR)
+    #define TARGET_SEQ_LEN_BLOCK_SIZE 16
     const int32_t B = getenv("B", 1);
-    const int32_t H = getenv("H", 28);
+    const int32_t Hq = getenv("Hq", 28);
+    const int32_t Hk = getenv("Hq", 7);
     const int32_t Lq = getenv("Lq", 8416);
     const int32_t Lk = getenv("Lk", 8416);
-    const int32_t S = getenv("S", 128);
-    const int32_t GQA_SIZE = getenv("GQA_SIZE", 4);
-    const int32_t SG_SCALE_FACTOR = getenv("SG_SCALE_FACTOR", 2);
-    const int32_t TARGET_SEQ_LEN_BLOCK_SIZE = getenv("TARGET_SEQ_LEN_BLOCK_SIZE", 16);
-    #define SUBGROUP_SIZE 16
 
-    const cl::NDRange GWS({B*H, int(Lq/TARGET_SEQ_LEN_BLOCK_SIZE), SG_SCALE_FACTOR*S});
+    assert(Hq % Hk == 0);     // implied
+    assert(S % SUBGROUP_SIZE == 0);     // implied
+    assert(TARGET_SEQ_LEN_BLOCK_SIZE == SUBGROUP_SIZE);   // implied
+
+    const cl::NDRange GWS({B*Hq, int(Lq/TARGET_SEQ_LEN_BLOCK_SIZE), SG_SCALE_FACTOR*S});
     const cl::NDRange LWS({1, 1, SG_SCALE_FACTOR*S});
 
     auto gws = GWS.get();
     auto lws = LWS.get();
     ECOUT("GWS={", *gws, ",", *(gws+1), ",", *(gws+2), "},",
           "LWS={", *lws, ",", *(lws+1), ",", *(lws+2), "}");
+    assert(SOURCE_SEQ_LEN_BLOCK_SIZE == *(lws+2));       // implied
     kernel.show_info(kernel_name.c_str(), LWS, SUBGROUP_SIZE);
 
-    tensorND<workitem_info> winfo({static_cast<size_t>(B*H), static_cast<size_t>(int(Lq/TARGET_SEQ_LEN_BLOCK_SIZE)), static_cast<size_t>(SG_SCALE_FACTOR*S)}, workitem_info());
+    tensorND<workitem_info> winfo({static_cast<size_t>(B*Hq), static_cast<size_t>(int(Lq/TARGET_SEQ_LEN_BLOCK_SIZE)), static_cast<size_t>(SG_SCALE_FACTOR*S)}, workitem_info());
     const std::vector<int32_t> shape_info_data = {
         // input0 query
-        B, H, 1, 1, 1, 1, Lq, S,
+        B, Hq, 1, 1, 1, 1, Lq, S,
         // input1 key
-        B, int(H/GQA_SIZE), 1, 1, 1, 1, Lk, S, 0, 0,
+        B, Hk, 1, 1, 1, 1, Lk, S, 0, 0,
         // input2 value
-        B, int(H/GQA_SIZE), 1, 1, 1, 1, Lk, S, 0, 0,
+        B, Hk, 1, 1, 1, 1, Lk, S, 0, 0,
         // input3 attn_mask
         1, 1, 1, 1, 1, 1, Lq, Lk,
         // input4 scale
         // output
-        B, H, 1, 1, 1, 1, Lq, S
+        B, Hq, 1, 1, 1, 1, Lq, S
     };
     tensorND<int32_t> shape_info;
     shape_info.resize(std::vector<size_t>{shape_info_data.size()}, std::vector<size_t>{1}, shape_info_data.data());
 
-    tensorND<cl_half> query({static_cast<size_t>(B), static_cast<size_t>(H), static_cast<size_t>(Lq), static_cast<size_t>(S)}, 1.0f);
-    tensorND<cl_half> key({static_cast<size_t>(B), static_cast<size_t>(H), static_cast<size_t>(Lk), static_cast<size_t>(S)}, 1.0f);
-    tensorND<cl_half> value({static_cast<size_t>(B), static_cast<size_t>(H), static_cast<size_t>(Lk), static_cast<size_t>(S)}, 1.0f);
+    tensorND<cl_half> query({static_cast<size_t>(B), static_cast<size_t>(Hq), static_cast<size_t>(Lq), static_cast<size_t>(S)}, 1.0f);
+    tensorND<cl_half> key({static_cast<size_t>(B), static_cast<size_t>(Hq), static_cast<size_t>(Lk), static_cast<size_t>(S)}, 1.0f);
+    tensorND<cl_half> value({static_cast<size_t>(B), static_cast<size_t>(Hq), static_cast<size_t>(Lk), static_cast<size_t>(S)}, 1.0f);
 
     tensorND<cl_half> scale({1}, 1.0f);
     tensorND<cl_half> attn_mask({static_cast<size_t>(B), 1, static_cast<size_t>(Lq), static_cast<size_t>(Lk)}, 0);
@@ -80,7 +87,7 @@ int main(void) {
     tensorND<cl_float> max_logits({4}, 0.0f);
     tensorND<cl_float> tmp_out({2}, 0.0f);
 
-    tensorND<cl_half> output({static_cast<size_t>(B), static_cast<size_t>(H), static_cast<size_t>(Lq), static_cast<size_t>(S)}, 0.0f);
+    tensorND<cl_half> output({static_cast<size_t>(B), static_cast<size_t>(Hq), static_cast<size_t>(Lq), static_cast<size_t>(S)}, 0.0f);
 
     cl::EnqueueArgs enqArgs(GWS, LWS);
     cl::Event evt;
@@ -118,8 +125,13 @@ int main(void) {
     ECOUT("CL_PROFILING_COMMAND_COMPLETE: +", delta_ns(evt.getProfilingInfo<CL_PROFILING_COMMAND_COMPLETE>()));
 
     auto latency_ns = (evt.getProfilingInfo<CL_PROFILING_COMMAND_END>() - evt.getProfilingInfo<CL_PROFILING_COMMAND_START>());
-    size_t num_ops_per_workitem = TARGET_SEQ_LEN_BLOCK_SIZE*Lk*2;
-    ECOUT("num_ops_per_workitem = ", (double)num_ops_per_workitem/1024/1024/1024, " GFLOPS, latency = ", (double)latency_ns / 1000 / 1000, " ms");
+    // size_t num_ops_per_workitem = 133120; //TARGET_SEQ_LEN_BLOCK_SIZE*Lk*2;
+    size_t num_ops_per_workitem = std::ceil(float(Lk)/SOURCE_SEQ_LEN_BLOCK_SIZE) * (S/SUBGROUP_SIZE) * (TARGET_SEQ_LEN_BLOCK_SIZE * SUBGROUP_SIZE);
+    num_ops_per_workitem *= 2;
+    ECOUT("num_ops_per_workitem = ", num_ops_per_workitem, ", latency = ", (double)latency_ns / 1000 / 1000, " ms");
+    ECOUT("[A770] vtune measured benchmark sdpa_model = 498ms latency mode, 194ms throughput mode, 31.9% XVE active, 68.1% stalled.");
+    ECOUT("[A770] vtune measured unittest sdpa model kernel = 77ms, 75.3% XVE active, 24.2% stalled.");
+    ECOUT("[A770] vtune measured QWen2 sdpa kernel = 82ms, 76.7% XVE active, 23.2% stalled.");
 
     if (nargs > 10) {
         winfo.to_cpu();
