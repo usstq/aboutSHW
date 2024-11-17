694f690c7ce1ea684f8165354ad6ed42af53eba3
Luwei Zhou <luwei.zhou@intel.com>
2024-11-17 15:28:57 +0800

    Update test xmx.

--- opencl/tests/test_xmx.py
+++ opencl/tests/test_xmx.py
@@ -37,8 +37,8 @@ __kernel void prepackB(__global half * src, __global half * dest) {
 
 // After prepacking, A and B can be blocked loaded.
 __attribute__((intel_reqd_sub_group_size(8)))
-__kernel void XMX_8x8_PREPACKED(__global half * A, __global half * B, __global float * C,  __global int *K) {
-    int k_loop = (*K)/ 16;
+__kernel void XMX_8x8_PREPACKED(__global half * A, __global half * B, __global float * C,  int K) {
+    int k_loop = (K)/ 16;
     float8 acc = 0.f;
     int channel = get_sub_group_local_id();
     int8 a, b;
@@ -101,6 +101,50 @@ __kernel void XMX_split_8x16(__global half * A, __global half * B, __global half
 #endif
 }
 
+__attribute__((intel_reqd_sub_group_size(8)))
+__kernel void XMX_split_8x32(__global half * A, __global half * B, __global half * C, int N) {
+    int sg = get_sub_group_id();
+    int channel = get_sub_group_local_id();
+    float8 acc=0.f;
+    float8 c0 =0.f;
+    float8 c1 =0.f;
+#if 1
+    int4 a = as_int4(intel_sub_group_block_read4((__global uint*)(A+sg%2*64)));
+#else
+    int8 a = as_int8(intel_sub_group_block_read8((__global uint*)(A)));
+#endif
+    __global half* b_sg_base = B+sg*256;
+    int8 b0 = *(__global int8*)(b_sg_base+16*channel);
+    int8 b1 = *(__global int8*)(b_sg_base+128+16*channel);
+#if 1
+    c0 = intel_sub_group_f16_f16_split_matrix_mad_k16(a, b0, acc);
+    c1 = intel_sub_group_f16_f16_split_matrix_mad_k16(a, b1, acc);
+#else
+    c0 = intel_sub_group_f16_f16_matrix_mad_k16(a, b0, acc);
+    c1 = intel_sub_group_f16_f16_matrix_mad_k16(a, b1, acc);
+#endif
+    half8 hc0 = convert_half8(c0);
+    half8 hc1 = convert_half8(c1);
+    __global half* C00 = C+sg*16+channel;
+    __global half* C01= C+sg*16+8+channel;
+    C00[0*N] = hc0.s0;
+    C00[1*N] = hc0.s1;
+    C00[2*N] = hc0.s2;
+    C00[3*N] = hc0.s3;
+    C00[4*N] = hc0.s4;
+    C00[5*N] = hc0.s5;
+    C00[6*N] = hc0.s6;
+    C00[7*N] = hc0.s7;
+
+    C01[0*N] = hc1.s0;
+    C01[1*N] = hc1.s1;
+    C01[2*N] = hc1.s2;
+    C01[3*N] = hc1.s3;
+    C01[4*N] = hc1.s4;
+    C01[5*N] = hc1.s5;
+    C01[6*N] = hc1.s6;
+    C01[7*N] = hc1.s7;   
+}
 '''
 kernels = cl.kernels(src, f"", "./dump/")
 # cl.profiling(True)
@@ -129,8 +173,8 @@ B = np.random.randint(-vRANGE, vRANGE+1, [K, N]).astype(np.float16)
 B_PACK = np.zeros([K, N]).astype(np.float16)
 A_PACK = np.zeros([M, K]).astype(np.float16)
 C =  np.matmul(A, B)
-K_tensor =  np.full((1), K, dtype=int)
-tK = cl.tensor(K_tensor)
+# K_tensor =  np.full((1), K, dtype=int)
+# tK = cl.tensor(K_tensor)
 tA = cl.tensor(A)
 tB = cl.tensor(B)
 tB_PACK = cl.tensor(B_PACK)
@@ -138,7 +182,7 @@ tA_PACK =cl.tensor(A_PACK)
 tC = cl.tensor([M, N], np.dtype(np.float32))
 kernels.enqueue("prepackA", [int(M), int(K/16)], [1, 1], tA, tA_PACK)
 kernels.enqueue("prepackB", tB.shape,tB.shape, tB, tB_PACK)
-kernels.enqueue("XMX_8x8_PREPACKED", [8],[8], tA_PACK, tB_PACK, tC, tK)
+kernels.enqueue("XMX_8x8_PREPACKED", [8],[8], tA_PACK, tB_PACK, tC, K)
 compare(C, tC.numpy())
 
 ## case 3: XMX output [8,16] , split on 2 subgroups
@@ -154,4 +198,20 @@ tB = cl.tensor(B)
 kernels.enqueue("XMX_split_8x16", [16],[16], tA, tB, tC)
 # cl.finish()
 compare(C, tC.numpy())
+
+## case 4: XMX output [8,32] , split on 2 subgroups
+M = 8
+N = 2048
+K = 16 
+A = np.random.randint(-vRANGE, vRANGE+1, [M, K]).astype(np.float16)
+B = np.random.randint(-vRANGE, vRANGE+1, [N, K]).astype(np.float16)
+C = np.matmul(A, B.transpose(1,0))
+tC = cl.tensor([M, N], np.dtype(np.float16))
+tA = cl.tensor(A)
+tB = cl.tensor(B)
+# there would be 1024/8=128 subgroups.
+kernels.enqueue("XMX_split_8x32", [int(N/2)],[int(N/2)], tA, tB, tC,N) 
+# cl.finish()
+compare(C, tC.numpy())
+print("-----------------")
 
