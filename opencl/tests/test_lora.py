9d40bafae2d56813dba419380cb50c2694c47e80
Luwei Zhou <luwei.zhou@intel.com>
2025-02-14 20:48:22 +0800

    gemm_B ref.

--- opencl/tests/test_lora.py
+++ opencl/tests/test_lora.py
@@ -34,6 +34,32 @@ def test_lora():
             sum += A[m*K+i]*B[i*RANK+sub_n];
         CC[m*N+n] = sum;
     }
+
+    __kernel void gemmB(__global half * input, __global half *B_Q,  __global half *B_K, __global half *B_V, __global half *CC, int rank) {
+        int wgs = get_num_groups(0);
+        int wg_id = get_group_id(0);
+        int lid = get_local_id(0);
+        int n_idx = get_global_id(0);
+
+        __global half *A = input;
+        __global half *BN = B_Q + n_idx;
+        int stride = TOKEN_HIDDEN_SZ;
+
+        if (wg_id ==  wgs - 2) {
+            A += rank;
+            BN = B_K + lid;
+            stride = KV_HIDDEN_SZ;
+        } else if (wg_id ==  wgs - 1) {
+            A += rank * 2;
+            BN = B_V + lid;
+            stride = KV_HIDDEN_SZ;
+        }
+
+        float sum = 0.f;
+        for (int i = 0; i < rank; i++)
+            sum += A[i]*BN[i*stride];
+        CC[n_idx] = sum;
+    }
     '''
     opt_src =  r'''
     __attribute__((intel_reqd_sub_group_size(SG_SZ)))
@@ -65,32 +91,35 @@ def test_lora():
         intel_sub_group_block_write_us((const __global ushort*)C_ptr, as_short(sum));
     }
     '''
-    MINICPM_HIDDEN_SZ = 1536
+    TOKEN_HIDDEN_SZ = 1536
+    KV_HIDDEN_SZ = 512
+    KV_GROUPS_SZ = TOKEN_HIDDEN_SZ // KV_HIDDEN_SZ
+
     RANK = 64
     N = RANK * 3
-    K = MINICPM_HIDDEN_SZ
+    K = TOKEN_HIDDEN_SZ
     # K = 16
     np.random.seed(0)
     vRANGE = 3
-    input = np.random.randint(-vRANGE, vRANGE+1, [1, K]).astype(np.float16)
-    wei = np.random.randint(-vRANGE, vRANGE+1, [K, N]).astype(np.float16)
+    inputA = np.random.randint(-vRANGE, vRANGE+1, [1, K]).astype(np.float16)
+    weiA = np.random.randint(-vRANGE, vRANGE+1, [K, N]).astype(np.float16)
     # C = np.matmul(A, B)
-    ref_ker = kernel_cache(reference, "")
+    ref_ker = kernel_cache(reference, options=f"-DTOKEN_HIDDEN_SZ={TOKEN_HIDDEN_SZ} -DKV_HIDDEN_SZ={KV_HIDDEN_SZ}")
     # Split [K, N]  to  3 [K, RANK]. Slicing would keep the original stride.So flatten and reshape.
-    weiQ = wei[:, 0:RANK].flatten().reshape(K, RANK)
-    weiK = wei[:, RANK:2*RANK].flatten().reshape(K, RANK)
-    weiV = wei[:, 2*RANK:N].flatten().reshape(K, RANK)
+    weiA_Q = weiA[:, 0:RANK].flatten().reshape(K, RANK)
+    weiA_K = weiA[:, RANK:2*RANK].flatten().reshape(K, RANK)
+    weiA_V = weiA[:, 2*RANK:N].flatten().reshape(K, RANK)
     tRefC = cl.tensor([1, N], np.dtype(np.float16))
     tC = cl.tensor([1, N], np.dtype(np.float16))
-    tInput = cl.tensor(input)
-    tWei = cl.tensor(wei)
-    tWeiQ = cl.tensor(weiQ)
-    tWeiK = cl.tensor(weiK)
-    tWeiV = cl.tensor(weiV)
+    tInput = cl.tensor(inputA)
+    tWeiA = cl.tensor(weiA)
+    tWeiA_Q = cl.tensor(weiA_Q)
+    tWeiA_K = cl.tensor(weiA_K)
+    tWeiA_V = cl.tensor(weiA_V)
 
-    ref_ker.enqueue("gemmA", [1, N],[1, RANK], tInput, tWei, tRefC, N, K)
+    ref_ker.enqueue("gemmA", [1, N],[1, RANK], tInput, tWeiA, tRefC, N, K)
     # Each WG would ouput [1, RANK]
-    ref_ker.enqueue("gemmA_with_split_wei", [1, N],[1, RANK], tInput, tWeiQ, tWeiK, tWeiV, tC, RANK, K, N)
+    ref_ker.enqueue("gemmA_with_split_wei", [1, N],[1, RANK], tInput, tWeiA_Q, tWeiA_K, tWeiA_V, tC, RANK, K, N)
     cl.finish()
     compare(tRefC.numpy(), tC.numpy())
     SG_SZ = 16
@@ -107,7 +136,7 @@ def test_lora():
     tTempC = cl.tensor([WG_NUM, N], np.dtype(np.float16))
     opt_kernel = kernel_cache(opt_src, options=f"-DSG_SZ={SG_SZ} -DK_PER_WG={K_PER_WG} -DRANK={RANK}")
     # N columns in one WG. The WG_NUM would separate K.
-    opt_kernel.enqueue("gemmA", [N*WG_NUM],[N], tInput, tWeiQ, tWeiK, tWeiV, tTempC, N, K)
+    opt_kernel.enqueue("gemmA", [N*WG_NUM],[N], tInput, tWeiA_Q, tWeiA_K, tWeiA_V, tTempC, N, K)
     cl.finish()
     C = np.zeros([1, N], dtype=np.float16)
     tempC = tTempC.numpy()
@@ -115,6 +144,29 @@ def test_lora():
         for j in range(WG_NUM):
             C[0][i] += tempC[j][i]
     compare(tRefC.numpy(), C)
+    # GEMM B
+    inputB = tRefC.numpy()
+    inputB_Q = inputB[:, 0:RANK].flatten().reshape(1, RANK)
+    inputB_K = inputB[:, RANK:2*RANK].flatten().reshape(1, RANK)
+    inputB_V = inputB[:, 2*RANK:N].flatten().reshape(1, RANK)
+
+    weiB_Q = np.random.randint(-vRANGE, vRANGE+1, [RANK, TOKEN_HIDDEN_SZ]).astype(np.float16)
+    weiB_K = np.random.randint(-vRANGE, vRANGE+1, [RANK, KV_HIDDEN_SZ]).astype(np.float16)
+    weiB_V = np.random.randint(-vRANGE, vRANGE+1, [RANK, KV_HIDDEN_SZ]).astype(np.float16)
+
+    ret_Q = np.matmul(inputB_Q, weiB_Q)
+    ret_K = np.matmul(inputB_K, weiB_K)
+    ret_V = np.matmul(inputB_V, weiB_V)
+    ret = np.concatenate((ret_Q, ret_K, ret_V), axis=1, dtype=np.float16)
+
+    tInputB = cl.tensor(inputB)
+    tWeiB_Q = cl.tensor(weiB_Q)
+    tWeiB_K = cl.tensor(weiB_K)
+    tWeiB_V = cl.tensor(weiB_V)
+    tRetRef = cl.tensor([1, TOKEN_HIDDEN_SZ+KV_HIDDEN_SZ*2], np.dtype(np.float16))
+    ref_ker.enqueue("gemmB", [TOKEN_HIDDEN_SZ+KV_HIDDEN_SZ*2],[KV_HIDDEN_SZ], tInputB, tWeiB_Q, tWeiB_K, tWeiB_V, tRetRef, RANK)
+    cl.finish()
+    compare(ret, tRetRef.numpy())
 
 # C = Matmul(A, B): A[1, 16], B [16, 16], C [1, 16]
 def test_FMA_basic():
