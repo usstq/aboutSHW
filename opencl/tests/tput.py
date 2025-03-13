bf3373501084acb9f0f552d8ed4471cfe8f21f6b
Luwei Zhou <luwei.zhou@intel.com>
2025-03-13 15:25:47 +0800

    Update.

--- opencl/tests/tput.py
+++ opencl/tests/tput.py
@@ -83,15 +83,44 @@ def test_FMA_basic(regM, regN):
 # GWS = [M//regM, N//(regN)]
 # LWS = [sgM, sgN * SG_SZ]
 # GWS:[256, 16], LWS:[16, 16], M:1024/64, N:64/64, K:1536/128
-def test_FMA_4x4(M, N, K,regM, regN, sgM, sgN):
+def gen_store_C(regM, regN):
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC), as_short(sum0_0));
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC+16), as_short(sum0_1));
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC+32), as_short(sum0_2));
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC+48), as_short(sum0_3));
+        # ptrC += N;
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC), as_short(sum1_0));
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC+16), as_short(sum1_1));
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC+32), as_short(sum1_2));
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC+48), as_short(sum1_3));
+        # ptrC += N;
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC), as_short(sum2_0));
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC+16), as_short(sum2_1));
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC+32), as_short(sum2_2));
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC+48), as_short(sum2_3));
+        # ptrC += N;
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC), as_short(sum3_0));
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC+16), as_short(sum3_1));
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC+32), as_short(sum3_2));
+        # intel_sub_group_block_write_us((__global ushort*)(ptrC+48), as_short(sum3_3));
+    src = r''''''
+    for m in range(regM):
+        for n in range(regN):
+            src +=  f"\t\t\tintel_sub_group_block_write_us((__global ushort*)(ptrC + SG_SZ * {n}), as_short(sum{m}_{n}));\n"
+        if m < regM - 1:
+            src += f"\t\t\tptrC += N;\n\n"
+            
+    return src
+def test_FMA_blocking(M, N, K,regM, regN, sgM, sgN):
+    func = f'gemm_rM{regM}_rN{regN}_sM{sgM}_sN{sgN}_{M}_N{N}_K{K}'
     src =  r'''
     __attribute__((intel_reqd_sub_group_size(SG_SZ)))
-    __kernel void gemm(__global half * A, __global half *B,  __global half *C, int M, int N, int K) {
+    __kernel void 
+    ''' + f'{func}' + r'''(__global half * A, __global half *B,  __global half *C, int M, int N, int K) {
         int sgid = get_sub_group_id();
         int sgid_N = sgid % sgN;
         int sgid_M = sgid / sgN;
-        int regM = 4;
-        int regN = 4;
+        ''' + f'int regM = {regM};\nint regN = {regN};\n' + r'''
         __local half lA[BM*BK];
         __local half lB[BK*BN];
         //__local half* ptrA = lA;
@@ -102,90 +131,37 @@ def test_FMA_4x4(M, N, K,regM, regN, sgM, sgN):
         __global half *ptrB = B + n_idx;
         __global half *ptrC = C + m_idx * N + n_idx;
 
+        '''  + "\n\t ".join([f"half sum{m}_{n} = 0;" for m in range(regM) for n in range(regN)]) + r''';
 
-        half sum0_0 = 0;
-        half sum0_1 = 0;
-        half sum0_2 = 0;
-        half sum0_3 = 0;
-        half sum1_0 = 0;
-        half sum1_1 = 0;
-        half sum1_2 = 0;
-        half sum1_3 = 0;
-        half sum2_0 = 0;
-        half sum2_1 = 0;
-        half sum2_2 = 0;
-        half sum2_3 = 0;
-        half sum3_0 = 0;
-        half sum3_1 = 0;
-        half sum3_2 = 0;
-        half sum3_3 = 0;
+        //for(int i = 0; i < K; i += BK) {
+        for(int i = 0; i < K; i += SG_SZ) {
 
-        for(int i = 0; i < K; i += BK) {
             // copy B & A into shared local mem
-            for(int j = 0; j < BK; j+=SG_SZ) {
-                ushort input0 = intel_sub_group_block_read_us((const __global ushort*)(ptrA + 0));
-                ushort input1 = intel_sub_group_block_read_us((const __global ushort*)(ptrA + K));
-                ushort input2 = intel_sub_group_block_read_us((const __global ushort*)(ptrA + 2*K));
-                ushort input3 = intel_sub_group_block_read_us((const __global ushort*)(ptrA + 3*K));
+            //for(int j = 0; j < BK; j+=SG_SZ) {
+                
+                '''  + "\n\t\t ".join([f"ushort input{m} = intel_sub_group_block_read_us((const __global ushort*)(ptrA + {m} * K));" for m in range(regM)]) + r'''
 
                 //__attribute__((opencl_unroll_hint))
                 for (int kk = 0; kk < SG_SZ; kk++) {
-                    half bb0 = as_half(intel_sub_group_block_read_us((const __global ushort*)(ptrB + 0)));
-                    half bb1 = as_half(intel_sub_group_block_read_us((const __global ushort*)(ptrB + 16)));
-                    half bb2 = as_half(intel_sub_group_block_read_us((const __global ushort*)(ptrB + 32)));
-                    half bb3 = as_half(intel_sub_group_block_read_us((const __global ushort*)(ptrB + 48)));
-                    half aa0 = as_half(intel_sub_group_broadcast(input0, kk));
-                    half aa1 = as_half(intel_sub_group_broadcast(input1, kk));
-                    half aa2 = as_half(intel_sub_group_broadcast(input2, kk));
-                    half aa3 = as_half(intel_sub_group_broadcast(input3, kk));
-                    sum0_0 = fma(aa0, bb0, sum0_0);
-                    sum0_1 = fma(aa0, bb1, sum0_1);
-                    sum0_2 = fma(aa0, bb2, sum0_2);
-                    sum0_3 = fma(aa0, bb3, sum0_3);
-                    sum1_0 = fma(aa1, bb0, sum1_0);
-                    sum1_1 = fma(aa1, bb1, sum1_1);
-                    sum1_2 = fma(aa1, bb2, sum1_2);
-                    sum1_3 = fma(aa1, bb3, sum1_3);
-                    sum2_0 = fma(aa2, bb0, sum2_0);
-                    sum2_1 = fma(aa2, bb1, sum2_1);
-                    sum2_2 = fma(aa2, bb2, sum2_2);
-                    sum2_3 = fma(aa2, bb3, sum2_3);
-                    sum3_0 = fma(aa3, bb0, sum3_0);
-                    sum3_1 = fma(aa3, bb1, sum3_1);
-                    sum3_2 = fma(aa3, bb2, sum3_2);
-                    sum3_3 = fma(aa3, bb3, sum3_3);
+            
+                     '''  + "\n\t\t\t ".join([f"half bb{n} = as_half(intel_sub_group_block_read_us((const __global ushort*)(ptrB + {n} * SG_SZ)));" for n in range(regN)]) + r'''
+                     '''  + "\n\t\t\t ".join([f"half aa{m} = as_half(intel_sub_group_broadcast(input{m}, kk));" for m in range(regM)]) + r'''
+                     ''' + "\n\t\t\t".join([f"sum{m}_{n} = fma(aa{m}, bb{n}, sum{m}_{n});" for m in range(regM) for n in range(regN)]) + r'''
                     ptrB += N;
                 }
                 ptrA +=SG_SZ;
-            }
+            //}
         }
-    
-        intel_sub_group_block_write_us((__global ushort*)(ptrC), as_short(sum0_0));
-        intel_sub_group_block_write_us((__global ushort*)(ptrC+16), as_short(sum0_1));
-        intel_sub_group_block_write_us((__global ushort*)(ptrC+32), as_short(sum0_2));
-        intel_sub_group_block_write_us((__global ushort*)(ptrC+48), as_short(sum0_3));
-        ptrC += N;
-        intel_sub_group_block_write_us((__global ushort*)(ptrC), as_short(sum1_0));
-        intel_sub_group_block_write_us((__global ushort*)(ptrC+16), as_short(sum1_1));
-        intel_sub_group_block_write_us((__global ushort*)(ptrC+32), as_short(sum1_2));
-        intel_sub_group_block_write_us((__global ushort*)(ptrC+48), as_short(sum1_3));
-        ptrC += N;
-        intel_sub_group_block_write_us((__global ushort*)(ptrC), as_short(sum2_0));
-        intel_sub_group_block_write_us((__global ushort*)(ptrC+16), as_short(sum2_1));
-        intel_sub_group_block_write_us((__global ushort*)(ptrC+32), as_short(sum2_2));
-        intel_sub_group_block_write_us((__global ushort*)(ptrC+48), as_short(sum2_3));
-        ptrC += N;
-        intel_sub_group_block_write_us((__global ushort*)(ptrC), as_short(sum3_0));
-        intel_sub_group_block_write_us((__global ushort*)(ptrC+16), as_short(sum3_1));
-        intel_sub_group_block_write_us((__global ushort*)(ptrC+32), as_short(sum3_2));
-        intel_sub_group_block_write_us((__global ushort*)(ptrC+48), as_short(sum3_3));
+
+        ''' +  gen_store_C(regM, regN) + r'''
     }
     '''
+    # print(src)
     cl.profiling(True)
 
     # np.random.seed(0)
     vRANGE = 1
-    REPEAT = 100
+    REPEAT = 20
     A = np.random.randint(-vRANGE, vRANGE, [M, K]).astype(np.float16)
     B = np.random.randint(-vRANGE, vRANGE+1, [K, N]).astype(np.float16)
     C_ref = np.matmul(A, B)
@@ -193,12 +169,12 @@ def test_FMA_4x4(M, N, K,regM, regN, sgM, sgN):
     tB_list = [cl.tensor(B) for _ in range(REPEAT)]
     tC_list = [cl.tensor([M, N], np.dtype(np.float16)) for _ in range(REPEAT)]
     SG_SZ = 16
-    BK= 128
+    BK= 64
     BM = regM*sgM
     BN = regN*sgN*SG_SZ
     assert M % BM == 0 , f'M:{M} is not multiple of BM:{BM}'
-    assert BK % SG_SZ == 0 , f'BK:{BK} is not multiple of SG_SZ:{SG_SZ}'
-    assert K % BK == 0 , f'BK:{BK} is not multiple of SG_SZ:{SG_SZ}'
+    # assert BK % SG_SZ == 0 , f'BK:{BK} is not multiple of SG_SZ:{SG_SZ}'
+    # assert K % BK == 0 , f'BK:{BK} is not multiple of SG_SZ:{SG_SZ}'
 
     kernel = kernel_cache(src, options=f"-DSG_SZ={SG_SZ} -DBK={BK} -DBM={BM} -DBN={BN} -DsgM={sgM} -DsgN={sgN}")
     GWS = [M//regM, N//(regN)]
@@ -206,7 +182,7 @@ def test_FMA_4x4(M, N, K,regM, regN, sgM, sgN):
     assert sgM *sgN * SG_SZ <= 1024, f" LWS:{LWS} exceed 1024 limitation"
 
     for i in range(0, REPEAT):
-        kernel.enqueue("gemm", GWS, LWS, tA_list[i], tB_list[i],tC_list[i], M, N, K)
+        kernel.enqueue(func, GWS, LWS, tA_list[0], tB_list[0],tC_list[0], M, N, K)
     ns = cl.finish()
     flops = M * N * K * 2
     for time in ns:
@@ -216,9 +192,20 @@ def test_FMA_4x4(M, N, K,regM, regN, sgM, sgN):
     print("----------------------------------------------------")
     compare(C_ref, tC_list[0].numpy())
 
+test_FMA_blocking(3072, 64, 1536, regM=8, regN=2, sgM=16, sgN=2)
+test_FMA_blocking(256, 64, 1536, regM=8, regN=2, sgM=16, sgN=2)
+test_FMA_blocking(256, 64, 1536, regM=2, regN=2, sgM=16, sgN=2)
+test_FMA_blocking(256, 64, 1536, regM=1, regN=1, sgM=16, sgN=4)
 
-# test_FMA_basic(4, 4)
-for n in (1536,):
-    for m in (3072,):
-        for k in (128,):
-            test_FMA_4x4(m, n, k, 4, 4, sgM=8, sgN=2)
+
+
+# GEMMA rank >=64, use [4,4]
+# for n in (64,128,192):
+#     for m in (3072,):
+#         for k in (1536,):
+#             test_FMA_blocking(m, n, k, regM=8, regN=2, sgM=16, sgN=2)
+
+# for k in (64,128,192):
+#     for m in (3072,):
+#         for n in (512,):
+#             test_FMA_blocking(m, n, k, regM=8, regN=2, sgM=16, sgN=1)
