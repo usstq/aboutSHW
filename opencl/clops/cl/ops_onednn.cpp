54184f8b5f29f447d6d0adca8c684a90afadbce7
Li, Tingqian <tingqian.li@intel.com>
2025-04-08 18:39:24 +0800

    add MLP

--- opencl/clops/cl/ops_onednn.cpp
+++ opencl/clops/cl/ops_onednn.cpp
@@ -38,15 +38,51 @@ public:
 };
 
 // https://uxlfoundation.github.io/oneDNN/dev_guide_matmul.html
-
-// 
-struct onednn_wmem {
+struct onednn_linear {
     memory weight;
     memory scale;
     memory zp;
+    matmul m_prim;
+    memory::dim m_K;
+    memory::dim m_N;
+    memory::data_type m_a_type;
+    dnnl::engine m_engine;
+    dnnl::stream m_stream;
+
+    onednn_linear() = default;
+
+    void forward(const tensor& a, tensor& c, tensor& bin_input) {
+        memory::dim M = a.get_shape()[0];
+        memory::desc rt_src_md = memory::desc(memory::dims({M, m_K}), m_a_type, memory::format_tag::ab);
+        memory::desc rt_dst_md = memory::desc(memory::dims({M, m_N}), m_a_type, memory::format_tag::ab);
+        memory::desc rt_bin_md = memory::desc(memory::dims({M, m_N}), m_a_type, memory::format_tag::ab);
+
+        auto src_mem = dnnl::ocl_interop::make_memory(rt_src_md, m_engine, ocl_interop::memory_kind::usm, (void *)(a));
+        //auto weights_mem = dnnl::ocl_interop::make_memory(m_weights_md, m_engine, ocl_interop::memory_kind::usm, (void *)(w));
+        auto dst_mem = dnnl::ocl_interop::make_memory(rt_dst_md, m_engine, ocl_interop::memory_kind::usm, (void *)(c));
+        //auto bias_mem = memory(bias_md, m_engine);
+
+        std::unordered_map<int, memory> args;
+        args.insert({DNNL_ARG_SRC, src_mem});
+        args.insert({DNNL_ARG_WEIGHTS, weight});
+        //args.insert({DNNL_ARG_BIAS, bias_mem});
+        args.insert({DNNL_ARG_DST, dst_mem});
+
+        if (scale) {
+            args.insert({DNNL_ARG_ATTR_SCALES | DNNL_ARG_WEIGHTS, scale});
+        }
+        if (zp) {
+            args.insert({DNNL_ARG_ATTR_ZERO_POINTS | DNNL_ARG_WEIGHTS, zp});
+        }
+        if (bin_input) {
+            auto bin_mem = dnnl::ocl_interop::make_memory(rt_bin_md, m_engine, ocl_interop::memory_kind::usm, (void *)(bin_input));
+            args.insert({DNNL_ARG_ATTR_MULTIPLE_POST_OP(0) | DNNL_ARG_SRC_1, bin_mem});
+        }
+        m_prim.execute(m_stream, args);
+    }
 };
 
-struct onednn_mm {
+struct onednn_matmul {
     matmul m_prim;
     memory::desc m_best_wei_md;
     memory::data_type m_w_type;
@@ -57,32 +93,37 @@ struct onednn_mm {
     dnnl::engine m_engine;
     dnnl::stream m_stream;
 
-
     // dtype: dtype of data
     // data:  f16/s8 [K, N]
     // scale: f32 [N, K//K_group_size]
     // zp   :  s8 [N, K//K_group_size]
-    onednn_wmem get_wmem(memory::data_type dtype, tensor& data, tensor& scale, tensor& zp) {
-        return to_wmem(dtype, static_cast<void*>(data), static_cast<void*>(scale), static_cast<void*>(zp));
+    onednn_linear get_linear(memory::data_type dtype, tensor& data, tensor& scale, tensor& zp) {
+        return to_linear(dtype, static_cast<void*>(data), static_cast<void*>(scale), static_cast<void*>(zp));
     }
 
-    onednn_wmem to_wmem(memory::data_type dtype, void* data, void* scale, void* zp) {
-        onednn_wmem wmem;
-        memory::desc raw_wei_md = memory::desc(memory::dims({m_K, m_N}), dtype, memory::format_tag::ab);
+    onednn_linear to_linear(memory::data_type dtype, void* data, void* scale, void* zp) {
+        onednn_linear linear;
+        linear.m_prim = m_prim;
+        linear.m_K = m_K;
+        linear.m_N = m_N;
+        linear.m_a_type = m_a_type;
+        linear.m_engine = m_engine;
+        linear.m_stream = m_stream;
+        memory::desc raw_wei_md = memory::desc(memory::dims({m_K, m_N}), dtype, memory::format_tag::ba);
 
         if (raw_wei_md != m_best_wei_md) {
-            wmem.weight = memory(m_best_wei_md, m_engine);
+            linear.weight = memory(m_best_wei_md, m_engine);
             std::cout << ">>>>>>>>>>>>>>>>>> weight layout changed : reorder is called (seems to be not working)" << std::endl;
             auto src_wei_mem = dnnl::ocl_interop::make_memory(
                                         raw_wei_md,
                                         m_engine,
                                         ocl_interop::memory_kind::usm,
                                         data);
-            reorder cvt(src_wei_mem, wmem.weight);
-            cvt.execute(m_stream, src_wei_mem, wmem.weight);
+            reorder cvt(src_wei_mem, linear.weight);
+            cvt.execute(m_stream, src_wei_mem, linear.weight);
             m_stream.wait();
         } else {
-            wmem.weight = dnnl::ocl_interop::make_memory(
+            linear.weight = dnnl::ocl_interop::make_memory(
                                         raw_wei_md,
                                         m_engine,
                                         ocl_interop::memory_kind::usm,
@@ -92,34 +133,36 @@ struct onednn_mm {
         if (scale) {
             // https://uxlfoundation.github.io/oneDNN/page_weights_decompression_matmul_cpp.html
             // Quantization Group size for scales. Must be divisible by 32.
-            auto wei_scale_md = memory::desc(memory::dims({m_K_groups, m_N}), memory::data_type::f32, memory::format_tag::ab);
-            wmem.scale = dnnl::ocl_interop::make_memory(wei_scale_md, m_engine, ocl_interop::memory_kind::usm, scale);
+            auto wei_scale_md = memory::desc(memory::dims({m_K_groups, m_N}), memory::data_type::f16, memory::format_tag::ab);
+            linear.scale = dnnl::ocl_interop::make_memory(wei_scale_md, m_engine, ocl_interop::memory_kind::usm, scale);
             if (zp) {
-                auto wei_zp_md = memory::desc(memory::dims({m_K_groups, m_N}), memory::data_type::s8, memory::format_tag::ab);
-                wmem.zp = dnnl::ocl_interop::make_memory(wei_zp_md, m_engine, ocl_interop::memory_kind::usm, zp);
+                auto wei_zp_md = memory::desc(memory::dims({m_K_groups, m_N}), m_w_type, memory::format_tag::ab);
+                linear.zp = dnnl::ocl_interop::make_memory(wei_zp_md, m_engine, ocl_interop::memory_kind::usm, zp);
             }
         }
-        return wmem;
+        return linear;
     }
 
-    static onednn_mm create(memory::data_type act_dtype, memory::data_type weight_dtype,
+    static onednn_matmul create(memory::data_type act_dtype, memory::data_type weight_dtype,
               int K,
               int N,
               int k_group_size,  // 0 means per-OC quantization
               bool with_wc_scales,
               bool with_wc_zp,
-              bool with_silu) {
-        return onednn_mm(act_dtype, weight_dtype, K, N, k_group_size, with_wc_scales, with_wc_zp, with_silu);
+              bool with_silu,
+              bool with_binmul) {
+        return onednn_matmul(act_dtype, weight_dtype, K, N, k_group_size, with_wc_scales, with_wc_zp, with_silu, with_binmul);
     }
 
-    onednn_mm(memory::data_type act_dtype,
+    onednn_matmul(memory::data_type act_dtype,
               memory::data_type weight_dtype,
               int K,
               int N,
               int k_group_size,  // 0 means per-OC quantization
               bool with_wc_scales,
               bool with_wc_zp,
-              bool with_silu) {
+              bool with_silu,
+              bool with_binmul) {
         m_a_type = act_dtype;
         m_w_type = weight_dtype;
         m_K_groups = 0;
@@ -141,6 +184,12 @@ struct onednn_mm {
         if (with_silu) {
             postops.append_eltwise(algorithm::eltwise_swish, alpha, beta);
         }
+        if (with_binmul) {
+            // using DNNL_RUNTIME_DIM_VAL as batch would report "could not append a binary post-op"
+            memory::dim fake_batch = 99999;
+            memory::desc bin_mul_md = memory::desc(memory::dims({fake_batch, m_N}), m_a_type, memory::format_tag::ab);
+            postops.append_binary(algorithm::binary_mul, bin_mul_md);
+        }
         primitive_attr attr;
 
         if (postops.len() > 0) {
@@ -151,13 +200,13 @@ struct onednn_mm {
             // Create attributes and indicate that the alpha and zero points are
             // runtime parameters
             // Set scales with multiple scales along K and N dimensions and with groups along K.
-            attr.set_scales(DNNL_ARG_WEIGHTS, (1 << 0) + (1 << 1), {k_group_size, 1}, memory::data_type::f32);
+            attr.set_scales(DNNL_ARG_WEIGHTS, (1 << 0) + (1 << 1), {k_group_size, 1}, memory::data_type::f16);
             // Set fpmath mode with `apply_to_int=true` to apply fpmath mode behavior to
             // integral primitives (in this example, matmul).
             attr.set_fpmath_mode(fpmath_mode::f16, true);
             if (with_wc_zp) {
-                // Set a single zero point with s8 data type.
-                attr.set_zero_points(DNNL_ARG_WEIGHTS, (1 << 0) + (1 << 1), {k_group_size, 1}, memory::data_type::s8);
+                // Set a single zero point with dtype same as weight_dtype.
+                attr.set_zero_points(DNNL_ARG_WEIGHTS, (1 << 0) + (1 << 1), {k_group_size, 1}, m_w_type);
             }
         }
 
@@ -170,36 +219,190 @@ struct onednn_mm {
         // Create the primitive.
         m_prim = matmul(matmul_pd);
     }
-
-    void forward(const tensor& a, const onednn_wmem& wmem, tensor& c) {
-        memory::dim M = a.get_shape()[0];
-        memory::desc rt_src_md = memory::desc(memory::dims({M, m_K}), m_a_type, memory::format_tag::ab);
-        memory::desc rt_dst_md = memory::desc(memory::dims({M, m_N}), m_a_type, memory::format_tag::ab);
-        auto src_mem = dnnl::ocl_interop::make_memory(rt_src_md, m_engine, ocl_interop::memory_kind::usm, (void *)(a));
-        //auto weights_mem = dnnl::ocl_interop::make_memory(m_weights_md, m_engine, ocl_interop::memory_kind::usm, (void *)(w));
-        auto dst_mem = dnnl::ocl_interop::make_memory(rt_dst_md, m_engine, ocl_interop::memory_kind::usm, (void *)(c));
-        //auto bias_mem = memory(bias_md, m_engine);
-
-        std::unordered_map<int, memory> args;
-        args.insert({DNNL_ARG_SRC, src_mem});
-        args.insert({DNNL_ARG_WEIGHTS, wmem.weight});
-        //args.insert({DNNL_ARG_BIAS, bias_mem});
-        args.insert({DNNL_ARG_DST, dst_mem});
-
-        if (wmem.scale) {
-            args.insert({DNNL_ARG_ATTR_SCALES | DNNL_ARG_WEIGHTS, wmem.scale});
-        }
-        if (wmem.zp) {
-            args.insert({DNNL_ARG_ATTR_ZERO_POINTS | DNNL_ARG_WEIGHTS, wmem.zp});
-        }
-
-        m_prim.execute(m_stream, args);
-    }
 };
 
 
+
+
+#if 0
+struct MoE {
+    onednn_matmul mlp_up;
+    onednn_matmul mlp_gate;
+    onednn_matmul mlp_down;
+    cl_kernels ocl_kernels;
+
+    cl_kernel slicing2d;
+    cl_kernel index_add_;
+
+    int m_hidden_size;
+
+    MoE() = default;
+    static MoE create(memory::data_type act_dtype,
+                      memory::data_type weight_dtype,
+                        int hidden_size,
+                        int moe_intermediate_size,
+                        int k_group_size) {
+        MoE moe;
+        mode.m_hidden_size = hidden_size;
+        bool is_quantized = (weight_dtype != memory::data_type::f16);
+        moe.mlp_up = onednn_matmul::create(act_dtype, weight_dtype, hidden_size, moe_intermediate_size, k_group_size, is_quantized, is_quantized, false, true);
+        moe.mlp_gate = onednn_matmul::create(act_dtype, weight_dtype, hidden_size, moe_intermediate_size, k_group_size, is_quantized, is_quantized, true, false);
+        moe.mlp_down = onednn_matmul::create(act_dtype, weight_dtype, moe_intermediate_size, hidden_size, k_group_size, is_quantized, is_quantized, false, false);
+        // load OCL code
+        std::ifstream t("moe_ocl.cl");
+        std::stringstream buffer;
+        buffer << t.rdbuf();
+        moe.ocl_kernels.setup(buffer.str(), "", "");
+        moe.slicing2d = moe.ocl_kernels.get_kernel("slicing2d");
+        moe.index_add_ = moe.ocl_kernels.get_kernel("index_add_");
+        return moe;
+    }
+
+    struct ExpertMLP {
+        onednn_linear up;
+        onednn_linear gate;
+        onednn_linear down;
+
+        // down_proj = self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x))
+        void forward(tensor& x, tensor& dst, tensor& temp_up, tensor& temp_gate) {
+            gate.forward(x, temp_gate, tensor{});
+            up.forward(x, temp_up, temp_gate);
+            down.forward(temp_up, dst);
+        }
+    };
+    std::vector<ExpertMLP> experts;
+
+    void set_expert(int index, memory::data_type dtype,
+                    tensor& gate_data, tensor& gate_scale, tensor& gate_zp,
+                    tensor& up_data, tensor& up_scale, tensor& up_zp
+                    tensor& down_data, tensor& down_scale, tensor& down_zp) {
+        if (experts.size() < index)
+            experts.resize(index);
+        expert[index].up = mlp_up.get_linear(dtype, gate_data, gate_scale, gate_zp);
+        expert[index].gate = mlp_gate.get_linear(dtype, up_data, up_scale, up_zp);
+        expert[index].down = mlp_down.get_linear(dtype, down_data, down_scale, down_zp);
+        return expert;
+    }
+/*
+
+    # Loop over all available experts in the model and perform the computation on each expert
+    for expert_idx in range(self.num_experts):
+        expert_layer = self.experts[expert_idx]
+        # idx    is the top-index,(top1, top2, ...) 
+        # top_x  is the token-index
+        #
+        idx, top_x = torch.where(expert_mask[expert_idx])
+        if idx.shape[0] != 0:
+            print("================expert_idx=", expert_idx)
+            print("expert_mask[expert_idx] ::::: ", expert_mask[expert_idx].shape)
+            print(expert_mask[expert_idx])
+            print("idx ::::: ", idx.shape, list(idx))
+            print("top_x ::::: ", top_x.shape, list(top_x))
+
+            # Index the correct hidden states and compute the expert hidden state for
+            # the current expert. We need to make sure to multiply the output hidden
+            # states by `routing_weights` on the corresponding tokens (top-1 and top-2)
+
+            # hidden_states[i,:]  [batch*seq, 2048]
+
+            current_state = hidden_states[None, top_x].reshape(-1, hidden_dim)
+            current_hidden_states = expert_layer(current_state) * routing_weights[top_x, idx, None]
+
+            # However `index_add_` only support torch tensors for indexing so we'll use
+            # the `top_x` tensor here.
+            final_hidden_states.index_add_(0, top_x, current_hidden_states.to(hidden_states.dtype))
+*/
+    memory alloc_mem() {
+        memory::desc md = memory::desc(dims, dtype, fmt);
+        memory ret = dnnl::ocl_interop::make_memory(md, onednn_context::engine(), ocl_interop::memory_kind::usm);
+        
+    }
+
+    void forward(tensor& hidden_states, tensor& expert_mask, tensor& routing_weights, tensor& final_hidden_states) {
+        //hidden_states
+        memory temp_up;
+        memory temp_gate;
+        memory current_state;
+        memory current_routing_w;
+        memory final_hidden_states;
+        for(int expert_idx = 0; expert_idx < experts.size(); expert_idx++) {
+            // check expert mask using CPU to rule-out experts
+            // expert_mask[expert_id, topk_id, token_id]
+            // expert_mask[i,j,k] == 1 means expert i is selected by token k as the top-j
+
+            // current_state: [total_tokens, m_hidden_size]
+            //      extract current_state from hidden_states according to expert_mask[i,:,:]
+            // current_routing_w : [total_tokens, 1]
+            //      extract current_routing_w from routing_weights
+            ocl_kernels.set_args(slicing2d, );
+            ocl_kernels.enqueue(slicing2d, {}, {});
+            experts[expert_idx].forward(current_state, current_state, temp_up, temp_gate);
+
+            ocl_kernels.set_args(index_add_, );
+            ocl_kernels.enqueue(index_add_, {}, {});
+            // scatter update current_state back to final_hidden_states
+        }
+    }
+};
+
+#endif
+
+memory to_memory(const py::array& b, memory::data_type dtype) {
+    // returns an instance of A that you made using B
+    py::buffer_info info = b.request();
+    memory::dims dims;
+    size_t numel = 1;
+
+    for(int i = 0; i < info.ndim; i++) {
+        numel *= info.shape[i];
+        dims.push_back(info.shape[i]);
+    }
+
+    auto host_dt = b.dtype();
+    auto* p_host = reinterpret_cast<uint8_t*>(info.ptr);
+
+    memory::format_tag fmt;
+    if (info.ndim == 1) fmt = memory::format_tag::a;
+    else if (info.ndim == 2) fmt = memory::format_tag::ab;
+    else if (info.ndim == 3) fmt = memory::format_tag::abc;
+    else if (info.ndim == 4) fmt = memory::format_tag::abcd;
+    else ASSERT(0);
+
+    memory::desc md = memory::desc(dims, dtype, fmt);
+    memory ret = dnnl::ocl_interop::make_memory(md, onednn_context::engine(), ocl_interop::memory_kind::usm);
+
+    sycl_queue.submit([&](sycl::handler& h) {
+        h.memcpy(ret.get_data_handle(), p_host, numel * host_dt.itemsize());
+    });
+    sycl_queue.wait();
+    return ret;
+}
+
+#if 0
+py::array tensor::to_numpy_f16(const memory& mem) {
+    // this shouldn't be a very frequent operation which requires optimizations
+    // so we just allocate
+    py::array ret(dt, shape);
+    py::buffer_info info = ret.request();
+    auto* p_host = reinterpret_cast<uint8_t*>(info.ptr);
+
+    // make sure data is ready
+    sycl_queue.submit([&](sycl::handler& h) {
+        h.memcpy(p_host, p_buff.get(), numel * dt.itemsize());
+    });
+    sycl_queue.wait();
+    return ret;
+}
+#endif
+
 void init_ops_onednn(py::module_& m) {
-    py::class_<onednn_wmem>(m, "onednn_wmem").def(py::init());
+    py::class_<onednn_linear>(m, "onednn_linear")
+        .def(py::init())
+        .def("forward", &onednn_linear::forward);
+
+    py::class_<memory>(m, "onednn_memory")
+        .def(py::init())
+        .def(py::init(&to_memory));
 
     py::enum_<memory::data_type>(m, "onednn_dtype", py::arithmetic())
         .value("s4", memory::data_type::s4)
@@ -209,9 +412,8 @@ void init_ops_onednn(py::module_& m) {
         .value("f16", memory::data_type::f16)
         .value("f32", memory::data_type::f32);
 
-    py::class_<onednn_mm>(m, "onednn_mm")
-        .def(py::init<>(&onednn_mm::create))
-        .def("get_wmem", &onednn_mm::get_wmem)
-        .def("forward", &onednn_mm::forward);
+    py::class_<onednn_matmul>(m, "onednn_matmul")
+        .def(py::init<>(&onednn_matmul::create))
+        .def("get_linear", &onednn_matmul::get_linear);
 }
 
--- opencl/tests/onednn-gemm.py
+++ opencl/tests/onednn-gemm.py
@@ -2,12 +2,28 @@
 from clops import cl
 import numpy as np
 import sys
+import torch
+
+
+def pack_i8_to_i4(B_q):
+    assert B_q.ndim == 2
+    K = B_q.shape[0]
+    N = B_q.shape[1]
+    B_q4 = np.zeros([K, N//2], dtype=np.int8)
+    for k in range(K):
+        for n in range(N//2):
+            even = (B_q[k,2*n]) & 0xF
+            odd = (B_q[k,2*n+1]) & 0xF
+            B_q4[k,n] = even | (odd << 4)
+    return B_q4
+
 
 def test_mm(M, K, N, K_group_size, w_dtype):
     np.random.seed(0)
     A = np.random.randint(-1,2,[M, K]).astype(np.float16)
     tA = cl.tensor(A)
     tC = cl.tensor(np.zeros([M, N], dtype=np.float16))
+    tP1 = cl.tensor()
 
     if w_dtype == cl.onednn_dtype.f16:
         B = np.random.randint(-1,2,[K, N]).astype(np.float16)
@@ -18,11 +34,13 @@ def test_mm(M, K, N, K_group_size, w_dtype):
         with_wc_scales = False
         with_wc_zp = False
         with_silu = False
-        mm = cl.onednn_mm(cl.onednn_dtype.f16, w_dtype, K, N, 0,
+        with_binmul = False
+        mm = cl.onednn_matmul(cl.onednn_dtype.f16, w_dtype, K, N, 0,
                             with_wc_scales,
                             with_wc_zp,
-                            with_silu)
-        wmem = mm.get_wmem(cl.onednn_dtype.f16, tB, cl.tensor(), cl.tensor())
+                            with_silu,
+                            with_binmul)
+        linear = mm.get_linear(cl.onednn_dtype.f16, tB, cl.tensor(), cl.tensor())
 
 
     if w_dtype == cl.onednn_dtype.s8:
@@ -44,39 +62,52 @@ def test_mm(M, K, N, K_group_size, w_dtype):
         with_wc_scales = True
         with_wc_zp = True
         with_silu = False
-        mm = cl.onednn_mm(cl.onednn_dtype.f16, w_dtype, K, N, K_group_size,
+        with_binmul = False
+        mm = cl.onednn_matmul(cl.onednn_dtype.f16, w_dtype, K, N, K_group_size,
                             with_wc_scales,
                             with_wc_zp,
-                            with_silu)
-        wmem = mm.get_wmem(cl.onednn_dtype.s8, tBq, tBs, tBz)
+                            with_silu,
+                            with_binmul)
+        linear = mm.get_linear(cl.onednn_dtype.s8, tBq, tBs, tBz)
 
 
-    if w_dtype == cl.onednn_dtype.s4:
+    if w_dtype == cl.onednn_dtype.u4:
         assert (K % K_group_size) == 0
         K_groups = K // K_group_size
-        B_q = np.random.randint(-1,2,[K, N]).astype(np.int8)
-        B_scale = np.random.randint(-4,5,[K_groups, N]).astype(np.float32)
-        B_zp = np.random.randint(-1,2,[K_groups, N]).astype(np.int8)
+        B_q = np.random.randint(0,3,[K, N]).astype(np.int8)
+        B_scale = np.random.randint(-4,5,[K_groups, N]).astype(np.float16)
+        B_zp = np.random.randint(0,3,[K_groups, N]).astype(np.int8)
 
+        BinarySrc = np.random.randint(-2,3,[M, N]).astype(np.float16)
+        
         B = (B_q.astype(np.float32) - B_zp.astype(np.float32).repeat(K_group_size, axis=0)) * B_scale.repeat(K_group_size, axis=0)
 
         C = A @ B
+        C = C * BinarySrc
         print("ref is calculated!")
 
-        tBq = cl.tensor(B_q)
+        # pack weight into 4bit
+        B_q4 = pack_i8_to_i4(B_q)
+        B_zp4 = pack_i8_to_i4(B_zp)
+
+        tBq4 = cl.tensor(B_q4)
         tBs = cl.tensor(B_scale)
-        tBz = cl.tensor(B_zp)
+        tBz = cl.tensor(B_zp4)
 
         with_wc_scales = True
         with_wc_zp = True
         with_silu = False
-        mm = cl.onednn_mm(cl.onednn_dtype.f16, w_dtype, K, N, K_group_size,
+        with_binmul = True
+        mm = cl.onednn_matmul(cl.onednn_dtype.f16, w_dtype, K, N, K_group_size,
                             with_wc_scales,
                             with_wc_zp,
-                            with_silu)
-        wmem = mm.get_wmem(cl.onednn_dtype.s8, tBq, tBs, tBz)
+                            with_silu,
+                            with_binmul)
+        linear = mm.get_linear(cl.onednn_dtype.u4, tBq4, tBs, tBz)
+        
+        tP1 = cl.tensor(BinarySrc)
 
-    mm.forward(tA, wmem, tC)
+    linear.forward(tA, tC, tP1)
 
     cl.finish()
 
@@ -86,6 +117,101 @@ def test_mm(M, K, N, K_group_size, w_dtype):
         print(C)
         print(C1)
 
+
+from torch import nn
+class config:
+    hidden_size = 2048
+    intermediate_size = 8192
+    num_experts = 4
+    num_experts_per_tok = 2
+    moe_intermediate_size = 768
+    num_hidden_layers = 1
+    sliding_window = 4096
+    num_attention_heads = 32
+    num_key_value_heads = 32
+    head_dim = 128
+    attention_bias = False
+    rms_norm_eps = 1e-6
+    attention_dropout = 0
+
+class Qwen3MoeMLP(nn.Module):
+    def __init__(self, config, intermediate_size=None):
+        super().__init__()
+        self.config = config
+        self.hidden_size = config.hidden_size
+        self.intermediate_size = intermediate_size if intermediate_size is not None else config.intermediate_size
+        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
+        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
+        self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=False)
+        self.act_fn = nn.SiLU()
+
+    def forward(self, x):
+        down_proj = self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x))
+        return down_proj
+
+
+
+
+def test_mlp(K_group_size, w_dtype):
+    mlp = Qwen3MoeMLP(config, intermediate_size = config.moe_intermediate_size)
+    mlp = mlp.eval()
+    is_quantized = w_dtype != cl.onednn_dtype.f16
+    with_wc_scales = is_quantized
+    with_wc_zp = is_quantized
+    with_silu = False
+    with_binmul = False
+
+    mlp_up = cl.onednn_matmul(cl.onednn_dtype.f16, w_dtype, config.hidden_size, config.moe_intermediate_size, K_group_size,
+                            with_wc_scales,
+                            with_wc_zp,
+                            with_silu,
+                            True)
+    mlp_gate = cl.onednn_matmul(cl.onednn_dtype.f16, w_dtype, config.hidden_size, config.moe_intermediate_size, K_group_size,
+                            with_wc_scales,
+                            with_wc_zp,
+                            True,
+                            with_binmul)
+
+    mlp_down = cl.onednn_matmul(cl.onednn_dtype.f16, w_dtype, config.moe_intermediate_size, config.hidden_size, K_group_size,
+                            with_wc_scales,
+                            with_wc_zp,
+                            with_silu,
+                            with_binmul)
+
+    linear_up = mlp_up.get_linear(cl.onednn_dtype.f32, cl.tensor(mlp.up_proj.weight.detach().numpy()), cl.tensor(), cl.tensor())
+    linear_gate = mlp_gate.get_linear(cl.onednn_dtype.f32, cl.tensor(mlp.gate_proj.weight.detach().numpy()), cl.tensor(), cl.tensor())
+    linear_down = mlp_down.get_linear(cl.onednn_dtype.f32, cl.tensor(mlp.down_proj.weight.detach().numpy()), cl.tensor(), cl.tensor())
+
+    M = 120
+
+    input = torch.randint(-1, 2, size=[M, config.hidden_size], dtype=torch.float16)
+    
+    x = cl.tensor(input.numpy())
+    temp_gate = cl.tensor(np.zeros([M, config.moe_intermediate_size], dtype=np.float16))
+    temp_up = cl.tensor(np.zeros([M, config.moe_intermediate_size], dtype=np.float16))
+    dst = cl.tensor(np.zeros([M, config.hidden_size], dtype=np.float16))
+
+    linear_gate.forward(x, temp_gate, cl.tensor())
+    linear_up.forward(x, temp_up, temp_gate)
+    linear_down.forward(temp_up, dst, cl.tensor())
+    cl.finish()
+
+    print(x.numpy())
+    print(temp_gate.numpy())
+    print(temp_up.numpy())
+    
+    dst_ref = mlp.forward(input.to(dtype=torch.float32))
+    
+    ref = dst_ref.detach().numpy()
+    act = dst.numpy()
+    if not np.allclose(ref, act):
+        print("============")
+        print(ref)
+        print("============")
+        print(act)
+
+np.set_printoptions(linewidth=1024)
 #test_mm(M = 1, K = 768, N = 2048, K_group_size = 0, w_dtype = cl.onednn_dtype.f16)
 #test_mm(M = 1, K = 768, N = 2048, K_group_size = 32, w_dtype = cl.onednn_dtype.s8)
-test_mm(M = 1, K = 768, N = 2048, K_group_size = 32, w_dtype = cl.onednn_dtype.s4)
+#test_mm(M = 1, K = 768, N = 2048, K_group_size = 32, w_dtype = cl.onednn_dtype.u4)
+test_mlp(K_group_size = 128, w_dtype = cl.onednn_dtype.f16)
