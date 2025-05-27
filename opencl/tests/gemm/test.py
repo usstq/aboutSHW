76e128af07a630eb04ce8aa0b8e8dfe805eaa18b
Luo Cheng <cheng.luo@intel.com>
2025-05-27 08:58:48 +0800

    add onednn test

--- opencl/tests/gemm/test.py
+++ opencl/tests/gemm/test.py
@@ -5,6 +5,15 @@ import time
 from clops import compare
 import os
 
+''' config for this file
+run:
+CM_FE_DIR=/mnt/luocheng/aboutSHW/opencl/cm python tests/gemm/test.py
+onednn:
+export OV_BUILD_PATH=/mnt/luocheng/2025.1/openvino/build
+with cliloader:
+export PATH=$PATH:/mnt/luocheng/opencl-intercept-layer/install/bin/
+CM_FE_DIR=`pwd`/cm cliloader -dv -cdt  --dump-dir ./dump/ python tests/gemm/test.py
+'''
 base_dir = os.path.dirname(os.path.abspath(__file__))
 files = ['kernel.hpp']
 src = [open(f'{base_dir}/{file}').read() for file in files]
@@ -71,12 +80,48 @@ C_SIZE = M*N*2//1024//1024
 ALL_SZIE = A_SIZE + B_SIZE + C_SIZE
 print(f'{M=} {N=} {K=} {A_SIZE=}MB {B_SIZE=}MB {C_SIZE=}MB {ALL_SZIE=}MB')
 print("-----------------------------------------------------------------------------")
-for i in range(0, 50):
-    kernels.enqueue("gemm", [M // BLOCK_SG_M, N // BLOCK_SG_N], [SG_M, SG_N], tA, tB_pack, tC, M, N, K, K, N, N)
-    ns = cl.finish()
-    flops = M * N * K * 2
-    repeat_size = (N/BLOCK_WG_N*M*K+K*N+M*N)*2
-    #repeat_size1 = (M*K+M/BLOCK_WG_M*K*N+M*N)*2
-    for time_opt in ns:
-        print(f'TPUT:{flops/time_opt:,.0f} GFLOPS, BW:{(M*K+K*N+M*N)*2/time_opt:,.0f}:{repeat_size/time_opt:,.0f} GB/s {time_opt*1e-3:,.0f} us')
+flops = M * N * K * 2
+repeat_size = (N/BLOCK_WG_N*M*K+K*N+M*N)*2
+if 1:
+    beg = time.time()
+    for i in range(0, 100):
+        kernels.enqueue("gemm", [M // BLOCK_SG_M, N // BLOCK_SG_N], [SG_M, SG_N], tA, tB_pack, tC, M, N, K, K, N, N)
+    cl.finish()
+    end = time.time()
+    time_opt = (end - beg) * 1e9 / 100
+    print(f'CUR    TPUT:{flops/time_opt:,.0f} GFLOPS, BW:{(M*K+K*N+M*N)*2/time_opt:,.0f}:{repeat_size/time_opt:,.0f} GB/s {time_opt*1e-3:,.0f} us')
+else:
+    for i in range(0, 50):
+        kernels.enqueue("gemm", [M // BLOCK_SG_M, N // BLOCK_SG_N], [SG_M, SG_N], tA, tB_pack, tC, M, N, K, K, N, N)
+        ns = cl.finish()
+        #repeat_size1 = (M*K+M/BLOCK_WG_M*K*N+M*N)*2
+        for time_opt in ns:
+            print(f'TPUT:{flops/time_opt:,.0f} GFLOPS, BW:{(M*K+K*N+M*N)*2/time_opt:,.0f}:{repeat_size/time_opt:,.0f} GB/s {time_opt*1e-3:,.0f} us')
 #print(f'loop M first {repeat_size//1024//1024/1024:.2f} GB loop N first {repeat_size1//1024//1024/1024:.2f} GB')
+
+def test_onednn(M, K, N, K_group_size, w_dtype):
+    tP1 = cl.tensor()
+
+    if w_dtype == cl.onednn_dtype.f16:
+        #B = np.random.randint(-1,2,[K, N]).astype(np.float16)
+        #C = A @ B
+        #print("ref is calculated!")
+
+        tB = cl.tensor(B.transpose().copy())
+        linear = cl.onednn_linear(cl.onednn_dtype.f16, w_dtype, M, K, N, -1, cl.onednn_matmul_type.none,
+                                  cl.onednn_dtype.f16, tB, cl.tensor(), cl.tensor())
+        linear.forward(tA, tC, tP1)
+
+        cl.finish()
+        #C_ref = np.matmul(A, B)
+        #compare(C_ref, tC.numpy())
+        beg = time.time()
+        for i in range(0, 100):
+            linear.forward(tA, tC, tP1)
+        ns = cl.finish()
+        end = time.time()
+        time_opt = (end - beg) * 1e9 / 100
+        print(f'ONEDNN TPUT:{flops/time_opt:,.0f} GFLOPS, BW:{(M*K+K*N+M*N)*2/time_opt:,.0f}:{repeat_size/time_opt:,.0f} GB/s {time_opt*1e-3:,.0f} us')
+
+if os.environ['OV_BUILD_PATH']:
+    test_onednn(M, K, N, 0, cl.onednn_dtype.f16)
