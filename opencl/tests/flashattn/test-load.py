f5b65c478917f1d5a12d0e8ec2d244936c9ec2f5
Luwei Zhou <luwei.zhou@intel.com>
2025-06-30 09:39:29 +0800

    quantized kernel.

--- opencl/tests/flashattn/test-load.py
+++ opencl/tests/flashattn/test-load.py
@@ -29,9 +29,8 @@ head_num = 28
 kvhead_num = 4
 head_sz = 128
 
-token_blk = 1
+grp_sz = head_num//kvhead_num
 local_sz=64
-token_per_wg=local_sz*token_blk
 low=-2
 high = 3
 
@@ -48,13 +47,15 @@ kint8_ref = torch.randint(low, high, [seq_len, kvhead_num, head_sz]).to(dtype=to
 qscale_ref = torch.zeros(seq_len, head_num).to(dtype=torch.float32)
 kscale_ref = torch.zeros(seq_len, kvhead_num).to(dtype=torch.float32)
 
-scale_out = torch.zeros((seq_len*head_num + token_blk -1)//token_blk*token_blk).to(dtype=torch.float32)
+qscale_out = torch.zeros(seq_len, head_num).to(dtype=torch.float32)
+kscale_out = torch.zeros(seq_len, kvhead_num).to(dtype=torch.float32)
+
 
 cl.profiling(True)
 
 src = r'''
 
-#pyeval f"#define TOKEN_NUM {token_blk}"
+#pyeval f"#define KVGRP_SZ {grp_sz}"
 #pyeval f"#define HEAD_SZ {head_sz}"
 #pyeval f"#define HEAD_NUM {head_num}"
 #pyeval f"#define KVHEAD_NUM {kvhead_num}"
@@ -88,45 +89,41 @@ void show_float(const matrix<T, M, N> mat) {
     printf("]\n");
 }
 
-extern "C" _GENX_MAIN_ void cm_test2d(SurfaceIndex base [[type("buffer_t")]], SurfaceIndex scale [[type("buffer_t")]]) {
+extern "C" _GENX_MAIN_ void cm_test2d(SurfaceIndex qkv [[type("buffer_t")]], SurfaceIndex qscale [[type("buffer_t")]], SurfaceIndex kscale [[type("buffer_t")]]) {
     auto id = cm_group_id(0)*cm_local_size(0) + cm_linear_local_id();
-    if (id*TOKEN_NUM >= HEAD_NUM*SEQ_LEN)
+    if (id >= KVHEAD_NUM*SEQ_LEN)
         return;
-    auto head = id % HEAD_NUM;
-    auto seq = id / HEAD_NUM;
+    auto headkv = id%KVHEAD_NUM;
+    auto head = id*KVGRP_SZ % HEAD_NUM;
+    auto seq = id / KVHEAD_NUM;
     auto pitch = HEAD_SZ*sizeof(half);
-    auto offset = (seq * (HEAD_NUM + KVHEAD_NUM + KVHEAD_NUM) + head)*pitch;
-    auto scale_off = id*TOKEN_NUM*sizeof(float);
+    auto qoff = (seq * (HEAD_NUM + KVHEAD_NUM + KVHEAD_NUM) + head)*pitch;
+    auto koff = (seq * (HEAD_NUM + KVHEAD_NUM + KVHEAD_NUM) + headkv + HEAD_NUM)*pitch;
 
-
-    auto remaining_tokens = (HEAD_NUM*SEQ_LEN-id*TOKEN_NUM) >= TOKEN_NUM ? TOKEN_NUM : (HEAD_NUM*SEQ_LEN-id*TOKEN_NUM);
+    auto kscale_off = id*sizeof(float);
+    auto qscale_off = id*KVGRP_SZ*sizeof(float);
 
     vector<half, HEAD_SZ> token;
-    vector<float, TOKEN_NUM> scaleV;
+    vector<float, 1> scaleV;
 
-    auto quan_token = token.format<int8_t,2, HEAD_SZ>().row(0);
+    auto quan_token= token.format<int8_t,2, HEAD_SZ>().row(0);
 
-    if (remaining_tokens == TOKEN_NUM) {
-        #pragma unroll
-        for(int i= 0;i<TOKEN_NUM;i++,offset+=pitch) {
-            token.format<uint32_t>() = cm_load<uint, HEAD_SZ/2>(base, offset);
-            half max=cm_reduced_max<half>(cm_abs(token));
-            quan_token =  cm_mul<int8_t>(token, (float)(127.0)/(float)(max));
-            cm_store<uint32_t, HEAD_SZ/4>(base, offset, quan_token.format<uint32_t>());
-            scaleV[i] = (float)(max)/127.0;
-        }
-    } else {
-        for(int i= 0;i<remaining_tokens;i++,offset+=pitch) {
-            token.format<uint32_t>() = cm_load<uint, HEAD_SZ/2>(base, offset);
-            //half max=cm_reduced_max<half>(cm_abs(token));
-            //quan_token =  cm_mul<int8_t>(token, float(127.0)/float(max));
-            cm_store<uint32_t, HEAD_SZ/4>(base, offset, quan_token.format<uint32_t>());
-            //scaleV[i] = (float)(max)/127.0;
-        }
-
+    #pragma unroll
+    for(int i= 0;i<KVGRP_SZ;i++,qoff+=pitch, qscale_off += sizeof(float)) {
+        token.format<uint32_t>() = cm_load<uint, HEAD_SZ/2>(qkv, qoff);
+        half max=cm_reduced_max<half>(cm_abs(token));
+        quan_token =  cm_mul<int8_t>(token, (float)(127.0)/(float)(max));
+        cm_store<uint32_t, HEAD_SZ/4>(qkv, qoff, quan_token.format<uint32_t>());
+        scaleV[0] = (float)(max)/127.0;
+        cm_store<uint32_t, 1>(qscale, qscale_off, scaleV.format<uint32_t>());
     }
-    cm_store<uint32_t, TOKEN_NUM>(scale, scale_off, scaleV.format<uint32_t>());
 
+    token.format<uint32_t>() = cm_load<uint, HEAD_SZ/2>(qkv, koff);
+    half max=cm_reduced_max<half>(cm_abs(token));
+    quan_token =  cm_mul<int8_t>(token, (float)(127.0)/(float)(max));
+    cm_store<uint32_t, HEAD_SZ/4>(qkv, koff, quan_token.format<uint32_t>());
+    scaleV[0] = (float)(max)/127.0;
+    cm_store<uint32_t, 1>(kscale, kscale_off, scaleV.format<uint32_t>());
 }
 
 '''
@@ -168,29 +165,35 @@ while mem_size < 4e9:
     mem_size += q.numel() * q.element_size()
 qkv =  torch.cat((q,k,v), 1)
 t_qkvlist = [cl.tensor(qkv.to(torch.float16).detach().numpy()) for _ in range(50)]
-t_qscaleList = [cl.tensor(scale_out.detach().numpy()) for _ in range(50)]
+t_qscaleList = [cl.tensor(qscale_out.detach().numpy()) for _ in range(50)]
+t_kscaleList = [cl.tensor(kscale_out.detach().numpy()) for _ in range(50)]
 
-print(f'head_num:{head_num}, seq_len:{seq_len}, token_per_wg:{token_per_wg}, token_blk:{token_blk}')
+
+print(f'head_num:{head_num}, seq_len:{seq_len}')
 
 cm_kernels = cl.kernels(pyeval(src), f"-cmc -mdump_asm -g2 ")
 
 lws = [local_sz]
-tokens_align_up=(head_num*seq_len+token_per_wg-1)//token_per_wg*local_sz
+tokens_align_up=(kvhead_num*seq_len+local_sz-1)//local_sz*local_sz
 gws = [tokens_align_up]
 print(f'GWS:{gws}, LWS:{lws}')
 for i in range(50):
-    cm_kernels.enqueue("cm_test2d", gws, lws, t_qkvlist[i], t_qscaleList[i])
+    cm_kernels.enqueue("cm_test2d", gws, lws, t_qkvlist[i], t_qscaleList[i], t_kscaleList[i])
 lat=cl.finish()
 
-rdbytes=seq_len*head_num*head_sz*2
+rdbytes=(seq_len*head_num*head_sz+seq_len*kvhead_num*head_sz)*2
 ns=sum(lat[5:])/len(lat[5:])
-print(f'avg latency:{ns*1e-3:.2f} us, read:{rdbytes/ns:.2f} GB/S, write:{(rdbytes/2+head_num*seq_len*4)/ns:.2f} GB/S')
+print(f'avg latency:{ns*1e-3:.2f} us, read:{rdbytes/ns:.2f} GB/S, write:{(rdbytes/2+(head_num+kvhead_num)*seq_len*4)/ns:.2f} GB/S')
 
 qint8_out=t_qkvlist[0].numpy()[:,0:head_num,0:head_sz//2].view(np.int8).reshape((seq_len, head_num, head_sz))
-qscale_out=t_qscaleList[0].numpy()[0:seq_len*head_num].view(np.float32).reshape((seq_len, head_num))
+kint8_out=t_qkvlist[0].numpy()[:,head_num:(head_num+kvhead_num),0:head_sz//2].view(np.int8).reshape((seq_len, kvhead_num, head_sz))
+
+# qscale_out=t_qscaleList[0].numpy()[0:seq_len*head_num].view(np.float32).reshape((seq_len, head_num))
 
 check_close(qint8_ref,torch.from_numpy(qint8_out))
-# check_close(qscale_ref,torch.from_numpy(qscale_out))
+check_close(kint8_ref,torch.from_numpy(kint8_out))
+check_close(qscale_ref,torch.from_numpy(t_qscaleList[0].numpy()))
+check_close(kscale_ref,torch.from_numpy(t_kscaleList[0].numpy()))
 
 
 cl.finish()
