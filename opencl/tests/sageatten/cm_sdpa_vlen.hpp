b627677ca1baf63f5ebd13289b326f2c308aee8a
Luwei Zhou <luwei.zhou@intel.com>
2025-07-14 11:03:48 +0800

    Add  sage attention based on cmflash attention.

---
+++ opencl/tests/sageatten/cm_sdpa_vlen.hpp
@@ -0,0 +1,224 @@
+
+#include "cm_sdpa_common.hpp"
+
+#ifdef CM_HAS_LSC_UNTYPED_2D
+#define USE_LSC 1
+#else
+#define USE_LSC 0
+#endif
+
+extern "C" _GENX_MAIN_ _GENX_FLOAT_CONTROL_(CM_RTE) void cm_quantize_qk(int seqlen, SurfaceIndex qkv [[type("buffer_t")]],
+                                            SurfaceIndex qscale [[type("buffer_t")]], SurfaceIndex kscale [[type("buffer_t")]], SurfaceIndex kmean_ptr [[type("buffer_t")]]) {
+    auto id = cm_group_id(0)*cm_local_size(0) + cm_linear_local_id();
+    if (id >= CMFLA_NUM_KV_HEADS*seqlen)
+        return;
+    constexpr int KVGRP_SZ =  CMFLA_NUM_HEADS / CMFLA_NUM_KV_HEADS;
+    auto headkv = id % CMFLA_NUM_KV_HEADS;
+    auto head = id * KVGRP_SZ % CMFLA_NUM_HEADS;
+    auto seq = id / CMFLA_NUM_KV_HEADS;
+    auto pitch = CMFLA_HEAD_SIZE*sizeof(half);
+    auto qoff = (seq * (CMFLA_NUM_HEADS + CMFLA_NUM_KV_HEADS+ CMFLA_NUM_KV_HEADS) + head)*pitch;
+    auto koff = (seq * (CMFLA_NUM_HEADS + CMFLA_NUM_KV_HEADS + CMFLA_NUM_KV_HEADS) + headkv + CMFLA_NUM_HEADS)*pitch;
+
+    auto kscale_off = (headkv*seqlen + seq)*sizeof(float);
+    auto qscale_off = (head*seqlen + seq)*sizeof(float);
+
+    vector<half, CMFLA_HEAD_SIZE> token;
+    vector<float, 1> scaleV;
+
+    auto quan_token= token.format<int8_t,2, CMFLA_HEAD_SIZE>().row(0);
+
+    #pragma unroll
+    for(int i= 0;i<KVGRP_SZ;i++,qoff+=pitch, qscale_off += sizeof(float)*seqlen) {
+        token.format<uint32_t>() = cm_load<uint, CMFLA_HEAD_SIZE/2>(qkv, qoff);
+        half max=cm_reduced_max<half>(cm_abs(token));
+        quan_token =  cm_mul<int8_t>(token, (float)(127.0)/(float)(max));
+        cm_store<uint32_t, CMFLA_HEAD_SIZE/4>(qkv, qoff, quan_token.format<uint32_t>());
+        scaleV[0] = (float)(max)/127.0;
+        cm_store<uint32_t, 1>(qscale, qscale_off, scaleV.format<uint32_t>());
+    }
+
+    vector<half, CMFLA_HEAD_SIZE> kmean;
+    token.format<uint32_t>() = cm_load<uint, CMFLA_HEAD_SIZE/2>(qkv, koff);
+    kmean.format<uint32_t>() = cm_load<uint, CMFLA_HEAD_SIZE/2>(kmean_ptr, headkv*pitch);
+    token -= kmean;
+    half max=cm_reduced_max<half>(cm_abs(token));
+    quan_token = cm_rnde<int8_t>(cm_mul<float>(token, (float)(127.0)/(float)(max)));
+    cm_store<uint32_t, CMFLA_HEAD_SIZE/4>(qkv, koff, quan_token.format<uint32_t>());
+    scaleV[0] = (float)(max)*scale_factor/float(127.0);
+    cm_store<uint32_t, 1>(kscale, kscale_off, scaleV.format<uint32_t>());
+}
+
+
+extern "C" _GENX_MAIN_ void Kmean(int seqlen, int seq_blk, half* k_ptr [[type("svmptr_t")]], half* kmean_ptr [[type("svmptr_t")]]) {
+
+    // q [B, L, H, S]
+    auto kvhead = cm_group_id(0);
+    auto sblk_idx = cm_group_id(1);
+    auto lid = cm_linear_local_id();
+    auto seq_start = lid * seq_blk;
+    uint constexpr TOTAL_HEADS = (CMFLA_NUM_KV_HEADS+CMFLA_NUM_KV_HEADS+CMFLA_NUM_HEADS);
+
+    auto threads_cnt = (seqlen + seq_blk - 1) / seq_blk;
+    //auto offset = ((seq_start * KVHEAD_NUM + kvhead)*HEAD_SZ + sblk_idx*CMFLA_STATE_BLK);
+    auto offset = ((seq_start *TOTAL_HEADS  + kvhead + CMFLA_NUM_HEADS)*CMFLA_HEAD_SIZE + sblk_idx*CMFLA_STATE_BLK);
+    k_ptr += offset;
+
+    constexpr uint BUF_SIZE = LOCAL_SZ*CMFLA_STATE_BLK*sizeof(float);
+    cm_slm_init(BUF_SIZE);
+    auto scratch_buf = cm_slm_alloc(BUF_SIZE);
+
+    vector <half, CMFLA_STATE_BLK> seq;
+    vector <float, CMFLA_STATE_BLK> seq_f32;
+    vector<float, CMFLA_STATE_BLK> seq_blk_sum = 0;
+    //don't know why, when lowering down pitch can achive 385.64 GB/S, just a test.
+    //TLB issue?
+    //auto pitch = (TOTAL_HEADS-1)*HEAD_SZ;
+    auto pitch = TOTAL_HEADS*CMFLA_HEAD_SIZE;
+
+    if (seq_start < seqlen) {
+        auto remaing_seq = (seq_start + seq_blk ) > seqlen ?  (seqlen-seq_start): seq_blk;
+
+        if (seq_blk == remaing_seq) {
+            #pragma unroll(UNROLL_NUM)
+            for (int i = 0; i < seq_blk; i++) {
+                cm_svm_block_read(reinterpret_cast<svmptr_t>(k_ptr), seq);
+                seq_f32 = seq;
+                seq_blk_sum += seq_f32;
+                k_ptr += pitch;
+            }
+        } else {
+            for (int i = 0; i < remaing_seq; i++) {
+                cm_svm_block_read(reinterpret_cast<svmptr_t>(k_ptr), seq);
+                seq_f32 = seq;
+                seq_blk_sum += seq_f32;
+                k_ptr += pitch;
+            }
+        }
+        cm_slm_block_write(scratch_buf, lid*CMFLA_STATE_BLK*sizeof(float), seq_blk_sum.format<float>());
+    }
+    cm_barrier();
+    if (lid == 0) {
+        seq_blk_sum = 0;
+        vector<float, CMFLA_STATE_BLK> tmpsum = 0;
+        int off = 0;
+        for (int r = 0; r<threads_cnt; r++, off +=CMFLA_STATE_BLK*sizeof(float)) {
+            cm_slm_block_read(scratch_buf, GENX_NONE, off, tmpsum.format<float>());
+            seq_blk_sum += tmpsum;
+        }
+        vector<half, CMFLA_STATE_BLK> kmean;
+        kmean = seq_blk_sum / (float)(seqlen);
+        cm_svm_block_write(reinterpret_cast<svmptr_t>(kmean_ptr + kvhead*CMFLA_HEAD_SIZE+sblk_idx*CMFLA_STATE_BLK), kmean);
+    }
+}
+
+extern "C" _GENX_MAIN_ void cm_sdpa_qkv_fused(
+    int seqlen,
+#if USE_LSC == 1
+    half* query_key_value [[type("svmptr_t")]],
+    float* dqscale_q [[type("svmptr_t")]],
+    float* dqscale_k [[type("svmptr_t")]],
+    half* output [[type("svmptr_t")]]
+#else
+    SurfaceIndex query_key_value [[type("buffer_t")]],
+    SurfaceIndex dqscale_q [[type("buffer_t")]],
+    SurfaceIndex dqscale_k [[type("buffer_t")]],
+    SurfaceIndex output [[type("buffer_t")]]
+#endif
+    ) {
+    constexpr int is_causal = CMFLA_IS_CAUSAL;
+    constexpr int num_heads = CMFLA_NUM_HEADS;
+    constexpr int head_size = CMFLA_HEAD_SIZE;
+    constexpr int num_kv_heads = CMFLA_NUM_KV_HEADS;
+
+    //# query [q_len, num_heads, S]
+    //#   key [kv_len, num_heads, S]
+    //# value [kv_len, num_heads, S]
+#if USE_LSC != 1
+    constexpr uint K_SLM_SIZE = (4*kv_step * head_size * sizeof(int8_t));
+    constexpr uint V_SLM_SIZE = (4*kv_step * head_size * sizeof(half));
+    constexpr uint Q_SLM_SIZE = 0;//(q_step * head_size * sizeof(half)) * local_size;
+
+    cm_slm_init(K_SLM_SIZE + V_SLM_SIZE + Q_SLM_SIZE);
+
+    auto slm_K = cm_slm_alloc(K_SLM_SIZE);
+    auto slm_V = cm_slm_alloc(V_SLM_SIZE);
+#endif
+
+    auto batch = cm_group_id(0);
+    auto h = cm_group_id(1);
+    auto hkv = h / (num_heads/num_kv_heads);
+    auto wg_id = cm_group_id(2); // each work-group handles a sequence
+    auto wg_local_id = cm_local_id(2);
+    int local_size = cm_local_size(2);
+
+    int q_start, kv_start, kv_seq_len, q_len;
+
+    // multiple work-groups are required to split a sequence,
+    // need to figure out which part of query-tokens to process
+    int wg_seq_len = local_size * q_step;
+    kv_start = 0;
+    kv_seq_len = seqlen;
+    q_start = (wg_id * local_size + wg_local_id) * q_step;
+    q_len = q_step;
+    if (q_start + q_len > seqlen) {
+        q_len = seqlen - q_start;
+    }
+    //printf("wg:%d.%d  q: %d, +%d   kv: %d, +%d\n", wg_id, wg_local_id, q_start, q_len, kv_start, kv_seq_len);
+
+    // qkv is fused
+    int kv_stop = kv_seq_len;
+    if (is_causal) {
+        kv_stop = (wg_id + 1) * wg_seq_len;
+        if (kv_stop > kv_seq_len) kv_stop = kv_seq_len;
+    }
+
+    // qkv fused
+    constexpr uint num_total_heads = num_heads + num_kv_heads * 2;
+    uint q_offset = (q_start*num_total_heads + h)*head_size;
+    uint k_offset = (kv_start*num_total_heads + num_heads + hkv)*head_size;
+    uint v_offset = (kv_start*num_total_heads + num_heads + num_kv_heads + hkv)*head_size;
+    uint o_offset = (q_start*num_heads + h)*head_size;
+    //# scale [head, sequence, 1]
+
+    uint qscale_offset =h*seqlen + q_start;
+    uint kscale_offset = hkv*seqlen;
+
+#if USE_LSC == 1
+    sdpa_kernel_lsc_prefetch<is_causal, num_heads, num_kv_heads, head_size, 1, 16>(
+                                wg_local_id,
+                                q_start, //q_start,
+                                kv_stop,
+                                q_len, //q_len,
+                                kv_seq_len, //kv_len,
+                                reinterpret_cast<svmptr_t>(query_key_value + q_offset),
+                                reinterpret_cast<svmptr_t>(query_key_value + k_offset),
+                                reinterpret_cast<svmptr_t>(query_key_value + v_offset),
+                                reinterpret_cast<svmptr_t>(dqscale_q + qscale_offset),
+                                reinterpret_cast<svmptr_t>(dqscale_k + kscale_offset),
+                                reinterpret_cast<svmptr_t>(output + o_offset));
+#else
+    sdpa_kernel<is_causal, num_heads, num_kv_heads, head_size, 1>(
+                                slm_K,
+                                slm_V,
+                                wg_local_id,
+                                local_size,
+                                q_start,
+                                kv_stop,
+                                q_len, //q_len,
+                                kv_seq_len, //kv_len,
+                                query_key_value,
+                                query_key_value,
+                                query_key_value,
+                                dqscale_q,
+                                dqscale_k,
+                                output,
+                                q_offset * sizeof(half),
+                                k_offset * sizeof(half),
+                                v_offset * sizeof(half),
+                                qscale_offset * sizeof(float),
+                                kscale_offset * sizeof(float),
+                                o_offset * sizeof(half),
+                                seqlen);
+#endif
+}
---
+++ opencl/tests/sageatten/cmfla.py
@@ -0,0 +1,240 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import functools
+
+from clops import cl
+import os
+
+import numpy as np
+
+def get_cm_grf_width():
+    cm_kernels = cl.kernels(r'''
+    extern "C" _GENX_MAIN_ void cm_get_grf_width(int * info [[type("svmptr_t")]]) {
+        info[0] = CM_GRF_WIDTH;
+    }''', f"-cmc")
+    t_info = cl.tensor([2], np.dtype(np.int32))
+    cm_kernels.enqueue("cm_get_grf_width", [1], [1], t_info)
+    return t_info.numpy()[0]
+
+CM_GRF_WIDTH = get_cm_grf_width()
+print(f'======================CM_GRF_WIDTH is {CM_GRF_WIDTH}')
+
+class flash_attn_cm:
+    def __init__(self, num_heads, num_kv_heads, head_size, is_causal = False, unroll_cnt=32):
+        self.num_heads = num_heads
+        self.num_kv_heads = num_kv_heads
+        self.head_size = head_size
+        self.is_causal = is_causal
+
+        self.unroll_cnt = unroll_cnt
+        self.state_blk_sz = 32
+        self.local_sz = 32 if CM_GRF_WIDTH == 512 else 64
+        src1 = r'''#include "cm_sdpa_vlen.hpp"'''
+        cwd = os.path.dirname(os.path.realpath(__file__))
+        print(f"compiling {cwd} {num_heads=} {head_size=} ...")
+
+        scale_factor = 1.0/(head_size**0.5)
+        self.kernels = cl.kernels(src1,
+                     (f'-cmc -Qxcm_jit_option="-abortonspill" -Qxcm_register_file_size=256  -mCM_printregusage -I{cwd}'
+                      f" -DCMFLA_NUM_HEADS={num_heads}"
+                      f" -DCMFLA_NUM_KV_HEADS={num_kv_heads}"
+                      f" -DCMFLA_HEAD_SIZE={head_size}"
+                      f" -DCMFLA_SCALE_FACTOR={scale_factor}"
+                      f" -DCMFLA_IS_CAUSAL={int(is_causal)}"
+                      f" -DUNROLL_NUM={int( self.unroll_cnt)}"
+                      f" -DCMFLA_STATE_BLK={int(self.state_blk_sz)}"
+                      f" -DLOCAL_SZ={int(self.local_sz)}"
+                      f" -mdump_asm -g2")
+                     )
+
+    def qkv_fused(self, qkv, n_repeats = 1):
+        seq_len, total_heads, head_size = qkv.shape
+        old_dtype = qkv.dtype
+        assert total_heads == (self.num_heads + self.num_kv_heads * 2)
+        assert head_size == self.head_size
+        t_qkv = [cl.tensor(qkv.to(torch.float16).detach().numpy()) for _ in range(n_repeats)]
+        t_out = cl.tensor([seq_len, self.num_heads, self.head_size], np.dtype(np.float16))
+
+        t_dqscale_q = [cl.tensor(torch.zeros(self.num_heads, seq_len).to(torch.float32).detach().numpy()) for _ in range(n_repeats)]
+        t_dqscale_k = [cl.tensor(torch.zeros(self.num_kv_heads, seq_len).to(torch.float32).detach().numpy()) for _ in range(n_repeats)]
+        t_mean_k = [cl.tensor(torch.zeros(1, self.num_kv_heads, self.head_size).to(torch.float16).detach().numpy()) for _ in range(n_repeats)]
+
+        local_sz = self.local_sz
+
+        kmean_seq_blk = (seq_len + local_sz - 1) // local_sz
+        kmean_seq_blk = (kmean_seq_blk + self.unroll_cnt - 1) // self.unroll_cnt * self.unroll_cnt
+        kmean_lws = [1, 1, local_sz]
+        kmean_gws = [self.num_kv_heads, self.head_size//self.state_blk_sz, local_sz]
+
+        quan_lws = [local_sz]
+        quan_gws=[(self.num_kv_heads*seq_len+local_sz-1)//local_sz*local_sz]
+        print(f'QUAN GWS:{quan_gws}, QUAN LWS:{quan_lws}')
+        print(f'KMEAN GWS:{kmean_gws}, KMEAN LWS:{kmean_lws}')
+
+        cl.finish()
+        for i in range(n_repeats):
+            self.kernels.enqueue("Kmean", kmean_gws, kmean_lws, seq_len, kmean_seq_blk, t_qkv[i], t_mean_k[i])
+            self.kernels.enqueue("cm_quantize_qk", quan_gws, quan_lws, seq_len, t_qkv[i], t_dqscale_q[i], t_dqscale_k[i], t_mean_k[i])
+        lat=cl.finish()
+        assert len(lat) == n_repeats * 2
+        lat_kmean=lat[0:n_repeats*2:2]
+        lat_quan=lat[1:n_repeats*2:2]
+        kmean_rdbytes = seq_len*self.num_kv_heads*self.head_size*2
+        quan_rdbytes=(seq_len*self.num_heads*self.head_size+seq_len*self.num_kv_heads*self.head_size)*2
+        if n_repeats > 10:
+            quan_ns=sum(lat_quan[5:])/len(lat_quan[5:])
+            kmean_ns=sum(lat_kmean[5:])/len(lat_kmean[5:])
+            print(f'[K_MEAN]: latency: {kmean_ns*1e-3:.2f} us, read:{kmean_rdbytes/kmean_ns:.2f} GB/S')
+            print(f'[QUAN_KV]: latency: {quan_ns*1e-3:.2f} us, read:{quan_rdbytes/quan_ns:.2f} GB/S, write:{(quan_rdbytes/2+(self.num_heads+self.num_kv_heads)*seq_len*4)/quan_ns:.2f} GB/S')
+
+        wg_size = 16
+        q_step = CM_GRF_WIDTH//32 # or 8 on Xe1
+        wg_seq_len = wg_size * q_step
+        wg_count = (seq_len + wg_seq_len - 1) // wg_seq_len
+        GWS = [1, self.num_heads, wg_count * wg_size]
+        LWS = [1, 1, wg_size]
+        print(f"calling qkv_fused {GWS=} {LWS=} x {n_repeats} times")
+        for i in range(n_repeats):
+            self.kernels.enqueue("cm_sdpa_qkv_fused", GWS, LWS, seq_len, t_qkv[i], t_dqscale_q[i], t_dqscale_k[i], t_out)
+        attn_output = torch.from_numpy(t_out.numpy()).to(old_dtype)
+        return attn_output
+
+    @staticmethod
+    @functools.cache
+    def create_instance(num_heads, num_kv_heads, head_size, is_causal):
+        return flash_attn_cm(num_heads, num_kv_heads, head_size, is_causal)
+
+def flash_attn_vlen_ref(q, k, v, cu_seqlens, is_causal = False):
+    seq_length, num_heads, head_size = q.shape
+    kv_seq_length, num_kv_heads, head_size = k.shape
+    old_dtype = q.dtype
+    q = q.transpose(0, 1)
+    k = k.transpose(0, 1)
+    v = v.transpose(0, 1)
+
+    # print(f"============2 {cu_seqlens=} {seq_length=} {num_heads=}")
+    # print(f"============2 {q.shape=} {q.is_contiguous()=} {k.shape=} {k.is_contiguous()=} {v.shape=} {v.is_contiguous()=}")
+    if is_causal:
+        attn_output = F.scaled_dot_product_attention(
+            q.unsqueeze(0).to(torch.float16), k.unsqueeze(0).to(torch.float16), v.unsqueeze(0).to(torch.float16),
+            is_causal = True,
+            dropout_p=0.0,
+            enable_gqa = (num_kv_heads != num_heads)
+        )
+    else:
+        attention_mask = torch.zeros([1, seq_length, seq_length], device=q.device, dtype=torch.bool)
+        if len(cu_seqlens):
+            for i in range(1, len(cu_seqlens)):
+                attention_mask[..., cu_seqlens[i - 1] : cu_seqlens[i], cu_seqlens[i - 1] : cu_seqlens[i]] = True
+        else:
+            attention_mask[...] = True
+
+        attn_output = F.scaled_dot_product_attention(
+            q.unsqueeze(0).to(torch.float16), k.unsqueeze(0).to(torch.float16), v.unsqueeze(0).to(torch.float16),
+            attn_mask = attention_mask,
+            dropout_p=0.0,
+            enable_gqa = (num_kv_heads != num_heads)
+        )
+    attn_output = attn_output.squeeze(0).transpose(0, 1)
+    # print(f"============2 {attn_output.shape=} ")
+    print(".")
+    return attn_output.to(old_dtype)
+
+
+def flash_attn(q, k, v, cu_seqlens):
+    _, num_heads, head_size = q.shape
+    func = flash_attn_cm.create_instance(num_heads, head_size)
+    # return flash_attn_vlen_ref(q, k, v, cu_seqlens)
+    return func(q, k, v, cu_seqlens)
+
+
+def check_close(input, other, atol=1e-3, rtol=1e-3):
+    print(f"[check_close] {input.shape}{input.dtype} vs {other.shape}{other.dtype}")
+    rtol_max = (((input - other).abs() - 1e-5)/other.abs())[other != 0].max()
+    atol_max = (((input - other).abs()) - 1e-5*other.abs()).max()
+    print(f"[check_close] rtol_max: {rtol_max}")
+    print(f"[check_close] atol_max: {atol_max}")
+    if not torch.allclose(input, other, atol=atol, rtol=rtol):
+        close_check = torch.isclose(input, other, atol=atol, rtol=rtol)
+        not_close_indices = torch.where(~close_check) # Invert the close check to find failures
+        print(f"Not close indices: {not_close_indices}")
+        print(f"    input_tensor: {input[not_close_indices]}")
+        print(f"    other_tensor: {other[not_close_indices]}")
+        assert 0
+
+def test_flash_attn_cm(seq_len, sub_seq_len, num_heads = 16, num_kv_heads = 16, head_size = 80):
+    cl.profiling(True)
+    torch.manual_seed(0)
+    torch.set_printoptions(linewidth=1024)
+
+    import numpy as np
+    q_len = kv_len = seq_len
+    cu_seqlens = torch.tensor([i for i in range(0, seq_len, sub_seq_len)] + [seq_len], dtype=torch.int32)
+    #print(cu_seqlens)
+
+    low = -1
+    high = 2
+    act_dtype = torch.float16
+    q = torch.randint(low, high, [q_len, num_heads, head_size]).to(dtype=act_dtype)
+    k = torch.randint(low, high, [kv_len, num_kv_heads, head_size]).to(dtype=act_dtype)
+    v = torch.randint(low, high, [kv_len, num_kv_heads, head_size]).to(dtype=act_dtype)/high
+
+    ref = flash_attn_vlen_ref(q, k, v, cu_seqlens)
+
+    func = flash_attn_cm.create_instance(num_heads, num_kv_heads, head_size, False)
+    out = func(q, k, v, cu_seqlens)
+    check_close(ref, out)
+
+    out = func(q, k, v, cu_seqlens, 100)
+    latency = cl.finish()
+    # for i,ns in enumerate(latency): print(f"[{i}]  {ns*1e-6:.3f} ms")
+    print(f" {seq_len=} {sub_seq_len=} average latency: {sum(latency[10:])/len(latency[10:])*1e-6:.3f} ms")
+
+
+def test_flash_attn_causal_batch1(seq_len, num_heads = 16, num_kv_heads = 16, head_size = 80):
+    cl.profiling(True)
+    torch.manual_seed(0)
+    torch.set_printoptions(linewidth=1024)
+
+    import numpy as np
+
+    low = -1
+    high = 3
+    act_dtype = torch.float16
+    is_causal = True
+    #manually produce different scales for different token.
+    q_factor = torch.randint(high, high+3, [seq_len, num_heads, head_size]).to(dtype=act_dtype)
+    k_factor = torch.randint(high, high+3, [seq_len, num_kv_heads, head_size]).to(dtype=act_dtype)
+    q_factor[:,:,0:head_size:3] = torch.randint(high+3,high+6,[1]).to(dtype=act_dtype)
+    k_factor[:,:,0:head_size:3] = torch.randint(high+3,high+6,[1]).to(dtype=act_dtype)
+
+    q = torch.randint(low, high, [seq_len, num_heads, head_size]).to(dtype=act_dtype) / q_factor
+    k = torch.randint(low, high, [seq_len, num_kv_heads, head_size]).to(dtype=act_dtype) / k_factor
+    # add bias to k to simulate outlier
+    bias_low=3
+    bias_high=7
+    step=3
+    k[:,:,0:head_size:step] += torch.randint(bias_low,bias_high,[1]).to(dtype=act_dtype)/bias_high
+
+
+    v = torch.randint(low, high, [seq_len, num_kv_heads, head_size]).to(dtype=act_dtype)/high
+    ref = flash_attn_vlen_ref(q, k, v, [], is_causal)
+
+    func = flash_attn_cm.create_instance(num_heads, num_kv_heads, head_size, is_causal)
+
+    qkv = torch.cat((q,k,v), 1)
+
+    out = func.qkv_fused(qkv)
+    out = func.qkv_fused(qkv, n_repeats=11)
+    latency = cl.finish()
+    # for i,ns in enumerate(latency): print(f"[{i}]  {ns*1e-6:.3f} ms")
+    print(f" qkv_fused_causal {seq_len=} average latency: {sum(latency[10:])/len(latency[10:])*1e-6:.3f} ms")
+    check_close(ref, out)
+
+if __name__ == "__main__":
+    # for seqlen in range(1025, 1055, 1):
+    #     test_flash_attn_causal_batch1(seqlen, num_heads = 28, num_kv_heads = 4, head_size = 128)
+    test_flash_attn_causal_batch1(113, num_heads = 28, num_kv_heads = 4, head_size = 128)
+    test_flash_attn_causal_batch1(1025, num_heads = 28, num_kv_heads = 4, head_size = 128)
