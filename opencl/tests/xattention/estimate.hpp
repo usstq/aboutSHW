749eb781f00270a8ea930c571440b9b37f3d46c1
Luo Cheng <cheng.luo@intel.com>
2025-07-29 19:34:58 +0800

    xattn: base [4096, 128k]: 84T/s, [8192,128k]: 95T/s(TODO: need reverse q)

---
+++ opencl/tests/xattention/estimate.hpp
@@ -0,0 +1,241 @@
+/*
+ * Copyright (c) 2020-2023, Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <cm/cm.h>
+
+#if defined(SHIM) || defined(CMRT_EMU)
+#define ATTR
+#define ATTR_BUF
+#define CM_LOCAL_BARRIER 0x20
+#include "emu/block2d.h"
+#else
+#define ATTR [[type("svmptr_t")]]
+#define ATTR_BUF [[type("buffer_t")]]
+#endif
+
+template <typename T1, typename T2>
+CM_INLINE void Transpose_8x8(matrix_ref<T1, 8, 8> in, matrix_ref<T2, 8, 8> out) {
+    matrix<T2, 8, 8> temp;
+    temp.row(0) = in.template select<2, 1, 4, 2>(0, 0);
+    temp.row(1) = in.template select<2, 1, 4, 2>(2, 0);
+    temp.row(2) = in.template select<2, 1, 4, 2>(4, 0);
+    temp.row(3) = in.template select<2, 1, 4, 2>(6, 0);
+    temp.row(4) = in.template select<2, 1, 4, 2>(0, 1);
+    temp.row(5) = in.template select<2, 1, 4, 2>(2, 1);
+    temp.row(6) = in.template select<2, 1, 4, 2>(4, 1);
+    temp.row(7) = in.template select<2, 1, 4, 2>(6, 1);
+
+    out.row(0) = temp.template select<4, 1, 2, 4>(0, 0);
+    out.row(2) = temp.template select<4, 1, 2, 4>(0, 1);
+    out.row(4) = temp.template select<4, 1, 2, 4>(0, 2);
+    out.row(6) = temp.template select<4, 1, 2, 4>(0, 3);
+    out.row(1) = temp.template select<4, 1, 2, 4>(4, 0);
+    out.row(3) = temp.template select<4, 1, 2, 4>(4, 1);
+    out.row(5) = temp.template select<4, 1, 2, 4>(4, 2);
+    out.row(7) = temp.template select<4, 1, 2, 4>(4, 3);
+}
+template <typename T, int N>
+CM_INLINE void read_1d(vector_ref<T, N> out, svmptr_t base) {
+    cm_ptr_block_read((T*)base, out);
+}
+
+template <typename T, int M, int N>
+CM_INLINE void read_2d(matrix_ref<T, M, N> out, svmptr_t base, uint pitch) {
+#pragma unroll
+    for (int i = 0; i < out.n_rows(); i++, base += pitch) {
+        cm_ptr_block_read((T*)base, out.row(i));
+    }
+}
+
+template <typename TSRC, int M, int N>
+CM_INLINE void write_2d(matrix_ref<TSRC, M, N> out, svmptr_t base, uint pitch) {
+#pragma unroll
+    for (int i = 0; i < out.n_rows(); i++, base += pitch) {
+        cm_ptr_block_write((TSRC*)base, out.row(i));
+    }
+}
+
+template <typename TSRC, int M, int N>
+CM_INLINE void write_2d(matrix_ref<TSRC, M, N> out, SurfaceIndex base, uint offset, uint pitch) {
+#pragma unroll
+    for (int i = 0; i < out.n_rows(); i++, offset += pitch) {
+        cm_store<int, N / (sizeof(int) / sizeof(TSRC)), DataSize::Default, CacheHint::WriteBack, CacheHint::WriteBack>(base, offset, out.row(i).format<int>());
+    }
+}
+
+CM_INLINE void gemm_qk_xe2(uint id_wg_m, uint id_wg_n, uint slm, svmptr_t src_a, svmptr_t src_b, svmptr_t dst, uint M, uint N, uint K, uint lda, uint ldb, uint ldc) {
+    constexpr int SG_SIZE = 16;
+    // sg blocking
+    constexpr int BLOCK_SG_M = 64;
+    constexpr int BLOCK_SG_N = 32;
+    constexpr int SG_M = 4;
+    constexpr int SG_N = 8;
+    constexpr int BLOCK_WG_K = 64;	// same in sg
+
+    // xehpg DPAS spec: dst: [8, 8], repeat: 1~8, depth: 8
+    static constexpr int REPEAT = 8;
+    static constexpr int DEPTH = 8;
+    static constexpr int BLOCK_REG_M = REPEAT;
+    static constexpr int BLOCK_REG_N = SG_SIZE;
+    static constexpr int BLOCK_DPAS_C = BLOCK_REG_M * BLOCK_REG_N;
+    static constexpr int VNNI = sizeof(half);
+    static constexpr int BLOCK_REG_K = DEPTH * sizeof(int) / VNNI;
+    static constexpr int BLOCK_REG_A = BLOCK_REG_M * BLOCK_REG_K;
+    static constexpr int BLOCK_REG_B = BLOCK_REG_N * BLOCK_REG_K;
+    static constexpr int BLOCK_WG_M = SG_M * BLOCK_SG_M;
+    static constexpr int BLOCK_WG_N = SG_N * BLOCK_SG_N;
+    // register blocking
+    static constexpr int REG_M = BLOCK_SG_M / BLOCK_REG_M;
+    static constexpr int REG_N = BLOCK_SG_N / BLOCK_REG_N;
+    static constexpr int REG_K = BLOCK_WG_K / BLOCK_REG_K;
+    static constexpr int REG_MN = REG_M * REG_N;
+
+    matrix<float, REG_M * REG_N, BLOCK_DPAS_C> acc = 0;
+    uint id_sg_n = cm_local_id(0);
+    uint id_sg_m = cm_local_id(1);
+    uint id_sg_mn = id_sg_m * SG_N + id_sg_n;
+
+    static_assert(REG_N == 2, "block_2d_desc for b is manually unrolled by 2");
+    // N[0:32]xK[0:16]
+    lsc::block_2d_desc<int, 1, BLOCK_REG_N, BLOCK_REG_K / 2> desc_b0{ src_b, N - 1, (uint)(K * sizeof(half) - 1), (uint)(K * sizeof(half) - 1),
+        0, (int)(id_wg_n * BLOCK_WG_N + id_sg_n * BLOCK_SG_N) };
+    // prefetch B
+    static constexpr int SG_MN = SG_M * SG_N;
+    lsc::block_2d_desc<half, 1, BLOCK_WG_N / SG_MN, 32> desc_prefetch_b{src_b, N - 1, (uint)(K * sizeof(half) - 1), (uint)(K * sizeof(half) - 1),
+        0, (int)(id_wg_n * BLOCK_WG_N + id_sg_mn * (BLOCK_WG_N / SG_MN)) };
+    // N[0:32]xK[0:16]                                                                   --> 4 regs
+    matrix<half, REG_N, BLOCK_REG_B> b0, b1;
+
+    // M[0:64]xK[0:32]
+    lsc::block_2d_desc<half, 2, 32, BLOCK_REG_K> desc_a0{ src_a, M - 1, (uint)(K * sizeof(half) - 1), (uint)(K * sizeof(half) - 1),
+        0, (int)(id_wg_m * BLOCK_WG_M + id_sg_m * BLOCK_SG_M) };
+    // M[32:64]xK[0:32]
+    // prefetch A
+    lsc::block_2d_desc<half, 1, BLOCK_WG_M / SG_MN, 32> desc_prefetch_a{ src_a, M - 1, (uint)(K * sizeof(half) - 1), (uint)(K * sizeof(half) - 1),
+        0, (int)(id_wg_m * BLOCK_WG_M + id_sg_mn * (BLOCK_WG_M / SG_MN)) };
+    // 0~3 is M[:]xK[0:16], 4~7 is M[:]xK[16:32]                                        --> 32+32=64 regs
+    matrix<half, 8, BLOCK_REG_A> a0, a1;
+
+    // warmup
+    // prefetch
+    cm_prefetch<CacheHint::Cached, CacheHint::Cached>(desc_prefetch_b);
+    desc_prefetch_b.set_block_x(desc_prefetch_b.get_block_x() + 32);
+    cm_prefetch<CacheHint::Cached, CacheHint::Cached>(desc_prefetch_a);
+    desc_prefetch_a.set_block_x(desc_prefetch_a.get_block_x() + 32);
+
+    // load b: N[0:16]xK[0:16]
+    cm_load<lsc::Transpose, CacheHint::Cached, CacheHint::Cached>(b0.row(0).format<int>(), desc_b0);
+    cm_load<lsc::Transpose, CacheHint::Cached, CacheHint::Cached, 0, 16>(b0.row(1).format<int>(), desc_b0);
+    desc_b0.set_block_x(desc_b0.get_block_x() + 8);
+    //cm_sbarrier(1);
+
+    auto dot = [&](matrix_ref<half, 4, BLOCK_REG_A> A0, matrix_ref<half, 4, BLOCK_REG_A> A1, matrix_ref<half, REG_N, BLOCK_REG_B> B) {
+#pragma unroll
+        for (int reg_n = 0; reg_n < REG_N; reg_n++) {
+#pragma unroll
+            for (uint reg_m = 0; reg_m < 4; reg_m++) {
+                acc.row((ushort)(reg_m * REG_N + reg_n)) = cm_dpas<CM_PRECISION_HF, CM_PRECISION_HF, 8, 8>(acc.row((ushort)(reg_m * REG_N + reg_n)),
+                    B.row((ushort)reg_n).format<int>(), A0.row((ushort)reg_m).format<int>());
+            }
+#pragma unroll
+            for (uint reg_m = 0; reg_m < 4; reg_m++) {
+                acc.row((ushort)((reg_m + 4) * REG_N + reg_n)) = cm_dpas<CM_PRECISION_HF, CM_PRECISION_HF, 8, 8>(acc.row((ushort)((reg_m + 4) * REG_N + reg_n)),
+                    B.row((ushort)reg_n).format<int>(), A1.row((ushort)reg_m).format<int>());
+            }
+        }
+    };
+
+    for (uint k = 0; k < K / BLOCK_WG_K; k++) {
+        cm_prefetch<CacheHint::Cached, CacheHint::Cached>(desc_prefetch_b);
+        cm_prefetch<CacheHint::Cached, CacheHint::Cached>(desc_prefetch_a);
+        desc_prefetch_b.set_block_x(desc_prefetch_b.get_block_x() + 32);
+        desc_prefetch_a.set_block_x(desc_prefetch_a.get_block_x() + 32);
+
+        // load b: N[0:16]xK[16:32]
+        cm_load<lsc::Transpose, CacheHint::Cached, CacheHint::Cached>(b1.row(0).format<int>(), desc_b0);
+        cm_load<lsc::Transpose, CacheHint::Cached, CacheHint::Cached, 0, 16>(b1.row(1).format<int>(), desc_b0);
+        desc_b0.set_block_x(desc_b0.get_block_x() + 8);
+
+        // load a: M[0:32]xK[0:32]
+        cm_load<lsc::Normal, CacheHint::Cached, CacheHint::Cached>(a0.format<half>(), desc_a0);
+        cm_load<lsc::Normal, CacheHint::Cached, CacheHint::Cached, 0, 32>(a1.format<half>(), desc_a0);
+        desc_a0.set_block_x(desc_a0.get_block_x() + 32);
+        // dot: (M[0:32]xK[0:16])*(N[0:16]xK[0:16])'
+        dot(a0.select<4, 1, BLOCK_REG_A, 1>(), a1.select<4, 1, BLOCK_REG_A, 1>(), b0.select_all());
+
+        // load b: N[0:16]xK[32:48]
+        cm_load<lsc::Transpose, CacheHint::Cached, CacheHint::Cached>(b0.row(0).format<int>(), desc_b0);
+        cm_load<lsc::Transpose, CacheHint::Cached, CacheHint::Cached, 0, 16>(b0.row(1).format<int>(), desc_b0);
+        desc_b0.set_block_x(desc_b0.get_block_x() + 8);
+        // dot: (M[0:32]xK[16:32])*(N[0:16]xK[16:32])'
+        dot(a0.select<4, 1, BLOCK_REG_A, 1>(4, 0), a1.select<4, 1, BLOCK_REG_A, 1>(4, 0), b1.select_all());
+    
+        cm_prefetch<CacheHint::Cached, CacheHint::Cached>(desc_prefetch_b);
+        cm_prefetch<CacheHint::Cached, CacheHint::Cached>(desc_prefetch_a);
+        desc_prefetch_b.set_block_x(desc_prefetch_b.get_block_x() + 32);
+        desc_prefetch_a.set_block_x(desc_prefetch_a.get_block_x() + 32);
+    
+        // load b: N[0:16]xK[48:64]
+        cm_load<lsc::Transpose, CacheHint::Cached, CacheHint::Cached>(b1.row(0).format<int>(), desc_b0);
+        cm_load<lsc::Transpose, CacheHint::Cached, CacheHint::Cached, 0, 16>(b1.row(1).format<int>(), desc_b0);
+        desc_b0.set_block_x(desc_b0.get_block_x() + 8);
+        // load a: M[0:32]xK[32:64]
+        cm_load<lsc::Normal, CacheHint::Cached, CacheHint::Cached>(a0.format<half>(), desc_a0);
+        cm_load<lsc::Normal, CacheHint::Cached, CacheHint::Cached, 0, 32>(a1.format<half>(), desc_a0);
+        desc_a0.set_block_x(desc_a0.get_block_x() + 32);
+        // dot: (M[0:32]xK[32:48])*(N[0:16]xK[32:48])'
+        dot(a0.select<4, 1, BLOCK_REG_A, 1>(), a1.select<4, 1, BLOCK_REG_A, 1>(), b0.select_all());
+
+        // load b: N[0:16]xK[0:16]
+        cm_load<lsc::Transpose, CacheHint::Cached, CacheHint::Cached>(b0.row(0).format<int>(), desc_b0);
+        cm_load<lsc::Transpose, CacheHint::Cached, CacheHint::Cached, 0, 16>(b0.row(1).format<int>(), desc_b0);
+        desc_b0.set_block_x(desc_b0.get_block_x() + 8);
+        // dot: (M[0:32]xK[48:64])*(N[0:16]xK[48:64])'
+        dot(a0.select<4, 1, BLOCK_REG_A, 1>(4, 0), a1.select<4, 1, BLOCK_REG_A, 1>(4, 0), b1.select_all());
+        //cm_sbarrier(0);
+        //cm_sbarrier(1);
+    }
+    
+    //cm_sbarrier(0);
+
+    // store
+    lsc::block_2d_desc<int, 1, 8, BLOCK_SG_N / 2> desc_c{ dst, M - 1, (uint)(N * sizeof(half) - 1), (uint)(N * sizeof(half) - 1),
+        (int)(id_wg_n * BLOCK_WG_N + id_sg_n * BLOCK_SG_N) / 2, (int)(id_wg_m * BLOCK_WG_M + id_sg_m * BLOCK_SG_M) };
+    matrix<half, REG_M * BLOCK_REG_M, REG_N * BLOCK_REG_N> tmp;
+#pragma unroll
+    for (uint reg_m = 0; reg_m < REG_M; reg_m++) {
+        // TODO(TUNE): merge in N dimension
+#pragma unroll
+        for (int reg_n = 0; reg_n < REG_N; reg_n++) {
+            tmp.select<BLOCK_REG_M, 1, BLOCK_REG_N, 1>(reg_m * BLOCK_REG_M, reg_n * BLOCK_REG_N) =
+                acc.row(reg_m * REG_N + reg_n);
+        }
+    }
+    cm_store<CacheHint::Uncached, CacheHint::WriteBack, 0, 8 * 0>(desc_c, tmp.select<BLOCK_REG_M, 1, REG_N * BLOCK_REG_N, 1>(0 * BLOCK_REG_M, 0).format<int>());
+    cm_store<CacheHint::Uncached, CacheHint::WriteBack, 0, 8 * 1>(desc_c, tmp.select<BLOCK_REG_M, 1, REG_N * BLOCK_REG_N, 1>(1 * BLOCK_REG_M, 0).format<int>());
+    cm_store<CacheHint::Uncached, CacheHint::WriteBack, 0, 8 * 2>(desc_c, tmp.select<BLOCK_REG_M, 1, REG_N * BLOCK_REG_N, 1>(2 * BLOCK_REG_M, 0).format<int>());
+    cm_store<CacheHint::Uncached, CacheHint::WriteBack, 0, 8 * 3>(desc_c, tmp.select<BLOCK_REG_M, 1, REG_N * BLOCK_REG_N, 1>(3 * BLOCK_REG_M, 0).format<int>());
+    cm_store<CacheHint::Uncached, CacheHint::WriteBack, 0, 8 * 4>(desc_c, tmp.select<BLOCK_REG_M, 1, REG_N * BLOCK_REG_N, 1>(4 * BLOCK_REG_M, 0).format<int>());
+    cm_store<CacheHint::Uncached, CacheHint::WriteBack, 0, 8 * 5>(desc_c, tmp.select<BLOCK_REG_M, 1, REG_N * BLOCK_REG_N, 1>(5 * BLOCK_REG_M, 0).format<int>());
+    cm_store<CacheHint::Uncached, CacheHint::WriteBack, 0, 8 * 6>(desc_c, tmp.select<BLOCK_REG_M, 1, REG_N * BLOCK_REG_N, 1>(6 * BLOCK_REG_M, 0).format<int>());
+    cm_store<CacheHint::Uncached, CacheHint::WriteBack, 0, 8 * 7>(desc_c, tmp.select<BLOCK_REG_M, 1, REG_N * BLOCK_REG_N, 1>(7 * BLOCK_REG_M, 0).format<int>());
+}
---
+++ opencl/tests/xattention/estimate.py
@@ -0,0 +1,234 @@
+#from xattn.src.utils import *
+import torch
+import math
+import torch.nn.functional as F
+
+import numpy as np
+
+from clops import cl
+import clops
+import time
+from clops import compare
+import os
+from clops.utils import Colors
+
+cl.profiling(True)
+
+def get_ref(Q: torch.Tensor, K: torch.Tensor, block_size, S, threshold=0.9, causal=True):
+    # Q, K, V shape = (B, H, L, d)
+    B, num_kv_head, k_len, d = K.shape
+    B, num_q_head, q_len, d = Q.shape
+    assert num_q_head % num_kv_head == 0
+    H = num_q_head
+    K = K.repeat_interleave(num_q_head // num_kv_head, 1)
+    
+    Q_block = (q_len + block_size -1) // block_size
+    K_block = (k_len + block_size -1) // block_size
+
+    # Antidiagonal Reshaping (Stride-S)
+    # TODO
+    #Q_resh = torch.cat([Q[:, :, S-1-i::S, :] for i in range(S)], dim=-1)
+    Q_resh = torch.cat([Q[:, :, i::S, :] for i in range(S)], dim=-1)
+    K_resh = torch.cat([K[:, :, i::S, :] for i in range(S)], dim=-1)
+
+    # Attention Estimation
+    A = Q_resh @ K_resh.transpose(-1, -2)
+    #print(f'{A.shape=} {Q_resh.shape=} {K_resh.shape=}')
+
+    #A = F.softmax(A / math.sqrt(d) / S, dim=-1, dtype=torch.float32).to(Q.dtype)
+    return A
+
+
+# q: [B, Hq, L_q, S]
+# k: [B, Hk, L_k, S]
+def test(q:torch.Tensor, k:torch.Tensor, block_size=128, threshold=0.9, stride=16, causal=True):
+    B, Hq, Lq, S = q.shape
+    _, Hk, Lk, _ = k.shape
+    M = Lq // stride
+    N = Lk // stride
+    K = stride * S
+
+    tQ = cl.tensor(q.detach().numpy())
+    tK = cl.tensor(k.detach().numpy())
+    tC = cl.tensor(np.zeros([B, Hq, M, N], np.float16))
+
+    # kernel
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    files = ['estimate.hpp']
+    src = [open(f'{base_dir}/{file}').read() for file in files]
+    src.append(
+    r'''
+    #define ABS(x) (x) < 0 ? -(x) : (x)
+
+    CM_INLINE void get_mn(uint& id_wg_m, uint& id_wg_n, uint M, uint N, int slice_no, int slice, const int BLOCK_WG_M, const int BLOCK_WG_N) {
+        uint id_wg_mn = cm_group_id(0);
+        if (slice_no == 0) {
+            if (slice == 0) {
+                // loop M first, N is shared, total = N/256*M+N
+                uint WG_MN = M / BLOCK_WG_M;
+                id_wg_m = id_wg_mn % WG_MN;
+                id_wg_n = id_wg_mn / WG_MN;
+            } else {
+                // loop N first, M is shared, total = M/128*N+M
+                uint WG_MN = N / BLOCK_WG_N;
+                id_wg_n = id_wg_mn % WG_MN;
+                id_wg_m = id_wg_mn / WG_MN;
+            }
+        } else {
+            uint wg_x = slice > 0 ? N / BLOCK_WG_N : M / BLOCK_WG_M;
+            uint slice_no_abs = ABS(slice_no);
+            uint slice_abs = ABS(slice);
+            int id_wg_mn_in_reminder = (int)id_wg_mn - (int)(slice_no_abs * slice_abs * wg_x);
+            uint slice_idx;
+            // in [slice_no x slice]
+            if (id_wg_mn_in_reminder < 0) {
+                slice_idx = id_wg_mn / (slice_abs * wg_x);
+                uint rem_in_slice = id_wg_mn % (slice_abs * wg_x);
+                uint x = rem_in_slice % slice_abs;
+                uint y = rem_in_slice / slice_abs;
+                id_wg_m = slice > 0 ? x + slice_idx * slice_abs : y;
+                id_wg_n = slice < 0 ? x + slice_idx * slice_abs : y;
+            } else {
+                uint slice_rem = slice_abs + (slice_no > 0 ? 1 : -1);
+                slice_idx = id_wg_mn_in_reminder / (slice_rem * wg_x);
+                uint rem_in_slice = id_wg_mn_in_reminder % (slice_rem * wg_x);
+                uint x = rem_in_slice % slice_rem;
+                uint y = rem_in_slice / slice_rem;
+                id_wg_m = slice > 0 ? x + slice_idx * slice_rem + slice_no_abs * slice_abs : y;
+                id_wg_n = slice < 0 ? x + slice_idx * slice_rem + slice_no_abs * slice_abs : y;
+            }
+        }
+    }
+
+    _GENX_MAIN_ void gemm_qk(svmptr_t src_a ATTR, svmptr_t src_b ATTR, svmptr_t dst ATTR, uint M, uint N, uint K, uint lda, uint ldb, uint ldc, int slice_no, int slice) {
+        const uint BLOCK_WG_M = 256;
+        const uint BLOCK_WG_N = 256;
+        const uint size_slm_b = 0;
+        auto slm = 0;
+        uint b_hq = cm_group_id(2);
+        uint b = b_hq / HQ;
+        uint hq = b_hq % HQ;
+        uint hk = hq / (HQ / HK);
+
+        static_assert(HQ % HK == 0, "HQ must be multiple of HK");
+
+        uint id_wg_m, id_wg_n;
+        get_mn(id_wg_m, id_wg_n, M, N, slice_no, slice, BLOCK_WG_M, BLOCK_WG_N);
+
+        src_a += (b * HQ + hq) * M * lda * (uint)sizeof(half);
+        src_b += (b * HK + hk) * N * ldb * (uint)sizeof(half);
+        dst += (b * HQ + hq) * M * ldc * (uint)sizeof(half);
+        gemm_qk_xe2(id_wg_m, id_wg_n, slm, src_a, src_b, dst, M, N, K, lda, ldb, ldc);
+    }
+    ''')
+    src = '\n'.join(src)
+
+    kernel_name = 'gemm_qk'
+    BLOCK_SG_M = 64
+    BLOCK_SG_N = 32
+    SG_M = 4
+    SG_N = 8
+    BLOCK_WG_M = BLOCK_SG_M * SG_M
+    BLOCK_WG_N = BLOCK_SG_N * SG_N
+
+    jit_option = '-abortonspill -noschedule '
+    kernels = cl.kernels(src, f'''-cmc -Qxcm_jit_option="{jit_option}" -Qxcm_register_file_size=256 -mCM_printregusage -mdump_asm -g2
+                       -DSTRIDE={stride} -DHQ={Hq} -DHK={Hk} -DHEAD_SIZE={S}''')
+    # loop N first:[0, 1], loop M first:[0, 0]; block M first[slice_no, slice(>0)], block N first[slice_no, slice(<0)]
+    #default linear
+    slice_no = 0
+    slice = N < M
+    if 1:
+        block_m = M // BLOCK_WG_M
+        block_n = N // BLOCK_WG_N
+        bias = BLOCK_WG_M / BLOCK_WG_N
+        devinfo = cl.dev_info()
+        eu_xecore = 8
+        xecores = devinfo["CL_DEVICE_MAX_COMPUTE_UNITS"] // eu_xecore
+        sm = math.sqrt(xecores * 2 / bias)
+        sn = math.sqrt(xecores * 2 * bias)
+        if 1 or N < M:
+            s0 = math.ceil(sn)
+            if block_n % s0 == 0:
+                slice = -s0
+                slice_no = block_n // slice
+            else:
+                if s0 * 1.5 < block_n:
+                    rem = block_n % s0
+                    expect_slice_no = (block_n + s0 - 1) // s0 - (s0 - rem)
+                    if expect_slice_no > 0:
+                        assert expect_slice_no >= 0, f'{block_n=} {expect_slice_no=} {s0=}'
+                        slice = -s0
+                        slice_no = -expect_slice_no
+        else:
+            # TODO: big weight matrix?
+            pass
+    slice_no = 0
+    slice = 0
+    print(f'{xecores=} {block_m=} {block_n=} {slice=} {slice_no=}')
+
+    for i in range(10):
+        kernels.enqueue(kernel_name, [M // BLOCK_WG_M * N // BLOCK_WG_N * SG_N, SG_M, B * Hq], [SG_N, SG_M, 1], tQ, tK, tC, M, N, K, K, K, N, slice_no, slice)
+    cl.finish()
+
+    C_ref = get_ref(q, k, block_size=block_size, S=stride, threshold=threshold, causal=causal)
+    compare(C_ref.detach().numpy(), tC.numpy())
+    print(f'{Colors.GREEN}passed{Colors.END}')
+    flops = B * Hq * M * N * K * 2
+    for i in range(0, 100):
+        kernels.enqueue(kernel_name, [M // BLOCK_WG_M * N // BLOCK_WG_N * SG_N, SG_M, B * Hq], [SG_N, SG_M, 1], tQ, tK, tC, M, N, K, K, K, N, slice_no, slice)
+    ns = cl.finish()
+    for i, time_opt in enumerate(ns):
+        print(f'(CUR)TPUT_{i}:{flops/time_opt:,.0f} GFLOPS, BW:{(M*K+K*N+M*N)*2/time_opt:,.0f} GB/s {time_opt*1e-3:,.0f} us')
+
+    #mask = torch.zeros([1, 1], dtype=torch.bool)
+    #return mask
+
+def main():
+    # 106 T/s:
+    # bsz = 1
+    # q_head = 1
+    # k_head = 1
+    # q_len = 1024*16*2
+    # k_len = 1024*128
+    # dim = 128
+    # block_size = 128
+    # stride = 16
+
+    # 84 T/s:
+    bsz = 1
+    q_head = 32
+    k_head = 4
+    q_len = 1024*4*1  # if q_len=1024*4*2 ==> 95 T/s
+    k_len = 1024*128
+    dim = 128
+    block_size = 128
+    stride = 16
+
+    assert (q_len // stride) >= 256 and (k_len // stride) >= 256, "minimum block size should be 256*256"
+
+    if True:
+        q = torch.randn((bsz,q_head,q_len,dim),dtype=torch.float32)
+        k = torch.randn((bsz,k_head,k_len,dim),dtype=torch.float32)
+
+        np.save("q.npy", q.numpy())
+        np.save("k.npy", k.numpy())
+    else:
+        q = torch.from_numpy(np.load("q.npy"))
+        k = torch.from_numpy(np.load("k.npy"))
+
+    q = q.to(torch.float16)
+    k = k.to(torch.float16)
+
+    test(q, k, block_size=block_size, threshold=0.9, stride=stride, causal=False)
+
+
+if __name__ == "__main__":
+    torch.manual_seed(3)
+    torch.set_printoptions(linewidth=1024)
+    
+    # requirements:
+    # num_q_head == num_kv_head
+    # chunk size alignment
+    # causal_mask
+    main()
