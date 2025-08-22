5ea33dd3284bd7760fbe35d78cb31d06421cb07a
River.Li <river.li@intel.com>
2025-08-22 10:20:31 +0800

    pa_kv_cache_update_ref works.

---
+++ opencl/tests/pageatten/pa_kv_cache_update_ref.hpp
@@ -0,0 +1,108 @@
+
+/*
+ * Copyright (c) 2020-2025, Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <cm/cm.h>
+#include <cm/cmtl.h>
+
+#ifndef ATTR
+#define ATTR [[type("svmptr_t")]]
+#define ATTR_BUF [[type("buffer_t")]]
+#endif
+
+constexpr uint wg_size = WG_SIZE;
+
+extern "C" _GENX_MAIN_ void pa_kv_cache_update(
+    const half* key [[type("svmptr_t")]],
+    const half* value [[type("svmptr_t")]],
+    const int32_t* past_lens [[type("svmptr_t")]],
+    const int32_t* block_indices [[type("svmptr_t")]],
+    const int32_t* block_indices_begins [[type("svmptr_t")]],
+    const int32_t* subsequence_begins [[type("svmptr_t")]],
+    half* key_cache [[type("svmptr_t")]],
+    half* value_cache [[type("svmptr_t")]],    
+    uint32_t key_pitch,
+    uint32_t value_pitch,
+    uint32_t batch_size_in_sequences) {
+    // # key:   [batch_size_in_tokens, num_kv_heads * k_head_size]
+    // # value  [batch_size_in_tokens, num_kv_heads * v_head_size]
+    // # key_cache:   [num_blocks, num_heads, block_size, k_head_size]
+    // # value_cache: [num_blocks, num_heads, block_size, v_head_size]
+    // 
+    // # past_lens: [sequences_num]
+    // # subsequence_begins: [sequences_num + 1]
+    // # block_indices: [used_blocks_num]
+    // # block_indices_begins: [sequences_num + 1]
+
+    // wg_count = aligned_to(batch_size_in_tokens, wg_size) // wg_size
+    // # GWS [1, num_heads, wg_count * wg_size]
+    // # LWS [1, 1, wg_size]
+
+    const auto head_idx = cm_group_id(1);
+    const auto wg_id = cm_group_id(2);
+    const auto wg_local_id = cm_local_id(2);
+    const auto local_size = cm_local_size(2);
+
+    // static_assert(local_size == wg_size);
+
+    // const uint token_idx = wg_id * local_size + wg_local_id;
+    const uint token_idx = cm_global_id(2);
+
+    // token_idx -> subsequence_idx
+    if (token_idx >= subsequence_begins[batch_size_in_sequences]) return;
+    uint subsequence_idx = 0;
+    for (uint i = 0; i < batch_size_in_sequences; i++) {
+        if (token_idx >= subsequence_begins[i] && token_idx < subsequence_begins[i + 1]) {
+            subsequence_idx = i;
+            break;
+        }
+    }
+    printf("wg:%d.%d, token_idx: %d, subsequence_idx: %d\n", wg_id, wg_local_id, token_idx, subsequence_idx);
+
+    const uint subsequence_begin_idx = subsequence_begins[subsequence_idx];
+
+    const uint past_len = past_lens[subsequence_idx];
+
+    const uint current_block_idx = (past_len + token_idx - subsequence_begin_idx) / PAGED_ATTENTION_BLOCK_SIZE;
+    const uint token_start_pos = (past_len + token_idx - subsequence_begin_idx) % PAGED_ATTENTION_BLOCK_SIZE;
+
+    const uint block_offset = block_indices_begins[subsequence_idx] + current_block_idx;
+
+    {
+        uint block_k_base_offset = (block_indices[block_offset] * KV_HEADS_NUM + head_idx) * ADJUSTED_K_HEAD_SIZE * PAGED_ATTENTION_BLOCK_SIZE;
+        uint key_out_offset = block_k_base_offset + token_start_pos * K_HEAD_SIZE;
+        uint key_in_offset = token_idx * key_pitch + head_idx * K_HEAD_SIZE;
+
+        vector<half, K_HEAD_SIZE> key_data;
+        key_data.format<int>() = cm_ptr_load<int, K_HEAD_SIZE / 2>((int*)key, key_in_offset * (int)sizeof(half));
+        cm_ptr_store<int, K_HEAD_SIZE / 2>((int*)key_cache, key_out_offset * (int)sizeof(half), key_data.format<int>());
+    }
+    {
+        uint block_v_base_offset = (block_indices[block_offset] * KV_HEADS_NUM + head_idx) * ADJUSTED_V_HEAD_SIZE * PAGED_ATTENTION_BLOCK_SIZE;
+        uint value_out_offset = block_v_base_offset + token_start_pos * V_HEAD_SIZE;
+        uint value_in_offset = token_idx * value_pitch + head_idx * V_HEAD_SIZE;
+
+        vector<half, V_HEAD_SIZE> value_data;
+        value_data.format<int>() = cm_ptr_load<int, V_HEAD_SIZE / 2>((int*)value, value_in_offset * (int)sizeof(half));
+        cm_ptr_store<int, V_HEAD_SIZE / 2>((int*)value_cache, value_out_offset * (int)sizeof(half), value_data.format<int>());
+    }
+}
---
+++ opencl/tests/pageatten/test_kvcache_update.py
@@ -0,0 +1,187 @@
+import os
+import time
+import math
+import torch
+import functools
+
+from clops import cl
+from clops import compare
+from clops.utils import Colors
+
+class pa_kvcache_update_cm:
+    def __init__(self, num_kv_heads, k_head_size, v_head_size, block_size):
+        self.num_kv_heads = num_kv_heads
+        self.k_head_size = k_head_size
+        self.v_head_size = v_head_size
+        self.block_size = block_size
+        self.wg_size = 16
+
+        src = r'''#include "pa_kv_cache_update_ref.hpp"'''
+        cwd = os.path.dirname(os.path.realpath(__file__))
+        print(f"compiling {cwd} {num_kv_heads=} {k_head_size=} {v_head_size=} ...")
+
+        jit_option = '-abortonspill -noschedule '
+        self.kernels = cl.kernels(src,
+                      (f' -cmc -Qxcm_jit_option="{jit_option}" -Qxcm_register_file_size=256 -mCM_printregusage -I{cwd}'
+                      f" -DKV_HEADS_NUM={num_kv_heads}"
+                      f" -DK_HEAD_SIZE={k_head_size}"
+                      f" -DV_HEAD_SIZE={v_head_size}"
+                      f" -DADJUSTED_K_HEAD_SIZE={k_head_size}"
+                      f" -DADJUSTED_V_HEAD_SIZE={v_head_size}"
+                      f" -DPAGED_ATTENTION_BLOCK_SIZE={self.block_size}"
+                      f" -DWG_SIZE={self.wg_size}"
+                      f" -mdump_asm -g2")
+                    )
+
+    def __call__(self, key:torch.Tensor,
+                 value:torch.Tensor,
+                key_cache:torch.Tensor,
+                value_cache:torch.Tensor,
+                past_lens:list,
+                subsequence_begins:list,
+                block_indices:list,
+                block_indices_begins:list,
+                n_repeats = 1):
+        batch_size_in_tokens, _ = key.shape
+        batch_size_in_sequences = len(past_lens)
+        key_pitch = key.stride()[0]
+        val_pitch = value.stride()[0]
+        print(f"============ {key.shape=} {key.is_contiguous()=}")
+        print(f"============ {value.shape=} {value.is_contiguous()=}")
+        print(f"============ {batch_size_in_tokens=} {batch_size_in_sequences=}")
+        print(f"============ {key.stride()=} {value.stride()=}")
+
+        t_key = cl.tensor(key.to(torch.float16).detach().numpy())
+        t_value = cl.tensor(value.to(torch.float16).detach().numpy())
+        t_key_cache = cl.tensor(key_cache.to(torch.float16).detach().numpy())
+        t_value_cache = cl.tensor(value_cache.to(torch.float16).detach().numpy())
+
+        t_block_indices=cl.tensor(torch.tensor(block_indices).to(torch.int32).detach().numpy())
+        t_past_lens=cl.tensor(torch.tensor(past_lens).to(torch.int32).detach().numpy())
+        t_block_indices_begins=cl.tensor(torch.tensor(block_indices_begins).to(torch.int32).detach().numpy())
+        t_subsequence_begins=cl.tensor(torch.tensor(subsequence_begins).to(torch.int32).detach().numpy())
+
+        wg_count = (batch_size_in_tokens + self.wg_size - 1) // self.wg_size
+        GWS = [1, self.num_kv_heads, int(wg_count * self.wg_size)]
+        LWS = [1, 1, self.wg_size]
+
+        for i in range(0, n_repeats):
+            print(f'{Colors.GREEN}calling pa_kv_cache_update {GWS=} {LWS=} {key_pitch=} {val_pitch=} {batch_size_in_sequences=} at {i}/{n_repeats} times {Colors.END}')
+            self.kernels.enqueue("pa_kv_cache_update", GWS, LWS, t_key, t_value,
+                            t_past_lens, t_block_indices, t_block_indices_begins, t_subsequence_begins, 
+                            t_key_cache, t_value_cache,
+                            key_pitch, val_pitch, batch_size_in_sequences)
+            ns = cl.finish()
+            for i, time_opt in enumerate(ns):
+                print(f'(pa_kv_cache_update)TPUT_{i}:[{key.numel()=}] [{value.numel()=}] {time_opt*1e-3:,.0f} us')
+
+        return t_key_cache.numpy(), t_value_cache.numpy()
+                    
+    @staticmethod
+    @functools.cache
+    def create_instance(num_kv_heads, k_head_size, v_head_size, block_size):
+        return pa_kvcache_update_cm(num_kv_heads, k_head_size, v_head_size, block_size)
+
+def test_pa_kv_cache_update(num_tokens:list, past_lens:list, num_kv_heads=1, k_head_size=64, v_head_size=64, block_size=16, check_perf=False):   
+    batch_size_in_sequences = len(num_tokens)
+    assert(batch_size_in_sequences == len(past_lens))
+
+    # prepare page attention inputs
+    key_data = []
+    value_data = []
+    subsequence_begins = []
+    block_indices_begins = []    
+    subsequence_begins.append(0)
+    block_indices_begins.append(0)
+    for i in range(batch_size_in_sequences):
+        subsequence_length = num_tokens[i] + past_lens[i]
+
+        k = torch.rand(subsequence_length, num_kv_heads*k_head_size).to(dtype=torch.float16)
+        v = torch.rand(subsequence_length, num_kv_heads*v_head_size).to(dtype=torch.float16)
+        key_data.append(k)
+        value_data.append(v)
+        
+        subsequence_start_pos = subsequence_begins[i]
+        subsequence_end_pos = subsequence_start_pos + num_tokens[i]
+        subsequence_begins.append(subsequence_end_pos)
+
+        required_blocks = (subsequence_length + block_size - 1) // block_size
+
+        block_indices_start_pos = block_indices_begins[i];
+        block_indices_end_pos = block_indices_start_pos + required_blocks;
+        block_indices_begins.append(block_indices_end_pos);
+
+    # simulate random block allocation
+    num_blocks = block_indices_begins[-1]
+    block_indices = torch.arange(num_blocks)
+    perm_idx = torch.randperm(block_indices.shape[0])
+    inv_per_idx = torch.argsort(perm_idx)
+    block_indices = block_indices[inv_per_idx]
+    print(f'{Colors.BLUE} ============ {subsequence_begins=} {Colors.END}')
+    print(f'{Colors.BLUE} ============ {block_indices_begins=} {Colors.END}')
+    print(f'{Colors.BLUE} ============ {block_indices=} {Colors.END}')
+
+    # generate key / value inputs
+    def get_kv_input(num_kv_heads, head_size, input_data):
+        batch_size_in_tokens = subsequence_begins[-1]
+        mem = torch.zeros(batch_size_in_tokens, num_kv_heads*head_size).to(torch.float16)
+        for i in range(batch_size_in_sequences):
+            mem[subsequence_begins[i] : subsequence_begins[i+1], :] = input_data[i][past_lens[i]:, :]
+        return mem
+    key = get_kv_input(num_kv_heads, k_head_size, key_data)
+    value = get_kv_input(num_kv_heads, v_head_size, value_data)
+    print(f'{Colors.BLUE} ============ {key.shape=} {key.is_contiguous()=}" {Colors.END}')
+    print(f'{Colors.BLUE} ============ {value.shape=} {value.is_contiguous()=}" {Colors.END}')
+    print(f'{Colors.BLUE} {key_data=} {Colors.END}')
+    print(f'{Colors.BLUE} {key=} {Colors.END}')
+    print(f'{Colors.BLUE} {value_data=} {Colors.END}')
+    print(f'{Colors.BLUE} {value=} {Colors.END}')
+    
+    # generate key_cache / value_cache
+    # input_data list of torch.Tensor with shape [subsequence_length, num_kv_heads, kv_head_size] for each sequence
+    def get_kv_cache(num_blocks, block_size, num_kv_heads, head_size, input_data, skip_input = True):
+        cache_data = torch.zeros(num_blocks, block_size, num_kv_heads*head_size).to(torch.float16)
+        for i in range(batch_size_in_sequences):
+            process_len = past_lens[i] if skip_input else past_lens[i] + num_tokens[i]
+            if process_len > 0:
+                blocks_num = (process_len + block_size - 1) // block_size
+                for block_idx in range(blocks_num):
+                    last_token_idx = process_len % block_size if block_idx == blocks_num -1 else block_size
+                    if last_token_idx == 0: last_token_idx = block_size
+                    print(f'{Colors.RED} {block_idx=} {blocks_num=} {process_len=} {last_token_idx=} {Colors.END}')
+                    for token_idx in range(last_token_idx):
+                        input_token_offset = block_idx * block_size + token_idx
+                        block_pos = block_indices[block_indices_begins[i] + block_idx]
+                        cache_data[block_pos, token_idx, :] = input_data[i][input_token_offset, :]
+        return cache_data.reshape(num_blocks, block_size, num_kv_heads, head_size).transpose(1, 2).contiguous()
+    key_cache = get_kv_cache(num_blocks, block_size, num_kv_heads, k_head_size, key_data)
+    value_cache = get_kv_cache(num_blocks, block_size, num_kv_heads, v_head_size, value_data)
+    print(f'{Colors.BLUE} ============ {key_cache.shape=} {key_cache.is_contiguous()=} {Colors.END}')
+    print(f'{Colors.BLUE} ============ {value_cache.shape=} {value_cache.is_contiguous()=} {Colors.END}')
+    # print(f'{Colors.BLUE} {key_cache=} {Colors.END}')
+    # print(f'{Colors.BLUE} {value_cache=} {Colors.END}')
+
+    # generate reference key/value cache
+    key_cache_ref = get_kv_cache(num_blocks, block_size, num_kv_heads, k_head_size, key_data, False)
+    value_cache_ref = get_kv_cache(num_blocks, block_size, num_kv_heads, v_head_size, value_data, False)
+    print(f'{Colors.BLUE} ============ {key_cache_ref.shape=} {key_cache_ref.is_contiguous()=} {Colors.END}')
+    print(f'{Colors.BLUE} ============ {value_cache_ref.shape=} {value_cache_ref.is_contiguous()=} {Colors.END}')
+    print(f'{Colors.BLUE} {key_cache_ref=} {Colors.END}')
+    print(f'{Colors.BLUE} {value_cache_ref=} {Colors.END}')
+    
+    # opt
+    pa_cm = pa_kvcache_update_cm.create_instance(num_kv_heads, k_head_size, v_head_size, block_size)
+    n_repeats = 100 if check_perf else 1
+    out_key_cache, out_value_cache = pa_cm(key, value, key_cache, value_cache, past_lens, subsequence_begins, block_indices, block_indices_begins, n_repeats)
+
+    compare(key_cache_ref.detach().numpy(), out_key_cache)
+    compare(value_cache_ref.detach().numpy(), out_value_cache)
+    print(f'{Colors.GREEN}kv_cache_update passed{Colors.END}')
+
+if __name__ == "__main__":
+    torch.manual_seed(3)
+    torch.set_printoptions(linewidth=1024)
+    
+    cl.profiling(True)
+    
+    test_pa_kv_cache_update([1024, 16, 17], [16, 0, 1])
