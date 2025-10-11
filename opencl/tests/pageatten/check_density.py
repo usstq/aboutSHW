4b3e7e0e2d6f20cbdc7616378d3f0b875c3180bc
ceciliapeng2011 <cecilia.peng@intel.com>
2025-10-11 13:15:53 +0800

    check_ov_density

--- opencl/tests/pageatten/check_density.py
+++ opencl/tests/pageatten/check_density.py
@@ -41,10 +41,8 @@ def check_cuda_density(base, dump_name):
 
     return density
     
-def check_ov_density(base):
-    pa_trunk_size = 4096
+def check_ov_density(base, prompt_len = 32*1024, pa_trunk_size = 4*1024):
     xattn_block_size = 128
-    prompt_len = 32*1024
     num_layers = 36
     num_heads = 32
     num_q_blocks = prompt_len // xattn_block_size
@@ -63,20 +61,46 @@ def check_ov_density(base):
             block_mask[l, :, i * cur_q_blocks : (i + 1) * cur_q_blocks, : cur_k_blocks] = layer_mask
 
     B, H, NQ, NL = block_mask.shape
-    densities = np.array([100.0 - count_false_percentage(block_mask[:, h, :, :].reshape(B, 1, NQ, NL)) for h in range (H)])
-    print(f'densities over heads {densities}')
-    densities = np.array([100.0 - count_false_percentage(block_mask[b, :, :, :].reshape(1, H, NQ, NL)) for b in range (B)])
-    print(f'densities over layers {densities}')
+    # densities = np.array([100.0 - count_false_percentage(block_mask[:, h, :, :].reshape(B, 1, NQ, NL)) for h in range (H)])
+    # print(f'densities over heads {densities}')
+    # densities = np.array([100.0 - count_false_percentage(block_mask[b, :, :, :].reshape(1, H, NQ, NL)) for b in range (B)])
+    # print(f'densities over layers {densities}')
     return 100.0 - count_false_percentage(block_mask)
 
 if __name__ == "__main__":
-    base = '/home/ceciliapeng/OCL/xattn-cuda/'
-    density = check_cuda_density(base, 'Qwen3-8B-demo-prompt-32k-16-0.9-int4lowbit.npy')
-    print(f'{Colors.GREEN}=============== density of CUDA with thresh 0.9: {density:.2f}%  ==============={Colors.END}\n')
+    # base = '/home/ceciliapeng/OCL/xattn-cuda/'
+    # density = check_cuda_density(base, 'Qwen3-8B-demo-prompt-32k-16-0.9-int4lowbit.npy')
+    # print(f'{Colors.GREEN}=============== density of CUDA with thresh 0.9: {density:.2f}%  ==============={Colors.END}\n')
     density = check_ov_density('/home/ceciliapeng/openvino/dump_thresh0.9.block_mask/')
     print(f'{Colors.BLUE}=============== density of OV with thresh 0.9: {density:.2f}%  ==============={Colors.END}\n')
 
-    density = check_cuda_density(base, 'Qwen3-8B-demo-prompt-32k-16-0.6-int4lowbit.npy')
-    print(f'{Colors.GREEN}=============== density of CUDA with thresh 0.6: {density:.2f}%  ==============={Colors.END}\n')
+    # density = check_cuda_density(base, 'Qwen3-8B-demo-prompt-32k-16-0.6-int4lowbit.npy')
+    # print(f'{Colors.GREEN}=============== density of CUDA with thresh 0.6: {density:.2f}%  ==============={Colors.END}\n')
     density = check_ov_density('/home/ceciliapeng/openvino/dump_thresh0.6.block_mask/')
     print(f'{Colors.BLUE}=============== density of OV with thresh 0.6: {density:.2f}%  ==============={Colors.END}\n')
+    
+    density = check_ov_density('/home/ceciliapeng/openvino/dump_thresh0.1.block_mask/')
+    print(f'{Colors.BLUE}=============== density of OV with thresh 0.1: {density:.2f}%  ==============={Colors.END}\n')
+    
+    density = check_ov_density('/home/ceciliapeng/openvino/dump_thresh0.1.block_mask.1trunk/', pa_trunk_size = 32*1024)
+    print(f'{Colors.BLUE}=============== density of OV with thresh 0.1 1trunk: {density:.2f}%  ==============={Colors.END}\n')
+
+    def get_all_last_subdir_paths(base_path):
+        last_subdirs = []
+        for root, dirs, files in os.walk(base_path):
+            if not dirs:  # Leaf directory
+                last_subdirs.append(root)
+        return last_subdirs
+
+    # check density of all subdirs
+    base_path = "/home/ceciliapeng/openvino/dump_block_mask"
+    last_subdir_paths = get_all_last_subdir_paths(base_path)
+    print("Full paths of last subdirectories:")
+    for path in last_subdir_paths:
+        print(f'{path}')
+        if "32k" in path:
+            density = check_ov_density(path, prompt_len = 32*1024, pa_trunk_size = 4*1024)
+            print(f'{Colors.YELLOW}=============== density of OV with {path}: {density:.2f}%  ==============={Colors.END}\n')
+        elif "64k" in path:
+            density = check_ov_density(path, prompt_len = 64*1024, pa_trunk_size = 4*1024)
+            print(f'{Colors.BLUE}=============== density of OV with {path}: {density:.2f}%  ==============={Colors.END}\n')
